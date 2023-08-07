4a6d488166b9c686b45bc6a00104d9ddea4df859
Nicoletti <callmenicoletti@gmail.com>
2023-08-07 14:02:41 -0300

    chore: Added new rule to fix carriage return (cr) problem in windows envirenment

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -7,6 +8,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App sim="nao" />
   </React.StrictMode>
 );
