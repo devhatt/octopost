d160606ebf92deaafa6aa3df6bb66b5e952d777f
Nicoletti <callmenicoletti@gmail.com>
2023-08-08 11:32:13 -0300

    fix:Removed some unused codes and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import scss from './App.module.scss';
 
 import Home from './pages/home/';
 
-interface ITest {
-  sim: string;
-}
-
-function App(role: ITest) {
-  const [sim, setSim] = useState('');
-
-  const ali = (opa: string) => {
-    setSim(opa);
-  };
-
+function App() {
   return (
-    <div className={scss.teste} id="#" onClick={() => ali('oi')}>
+    <div className={scss.teste}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-      <p>Hello World! SIM?</p> {role.sim} {sim}
     </div>
   );
 }
