9295d4e98c387d0cc3d9bbaeb88b351f9ed9180d
Nicoletti <callmenicoletti@gmail.com>
2023-08-08 11:32:13 -0300

    fix:Removed some unused codes and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import scss from './App.module.scss';
 
-import './styles/base.scss';
+import '~styles/base.scss';
+import Home from './pages/home/';
 
 function App() {
   return (
-    <div className={scss.mainContainer}>
-      <div className={scss.gridContainer}>
-        <div className={scss.gridSwitches} />
-        <div className={scss.gridInput} />
-        <div className={scss.gridTabs} />
-      </div>
+    <div className={scss.teste}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,12 @@
 import scss from './index.module.scss';
 
-import CharacterCountdown from '../../components/CharacterCountDown';
-
 const Home = () => {
   return (
-    <div className={scss.Home}>
-      <h1>Octopus</h1>
-      <img className={scss.Logo} src="/logo.png" />
-
-      <div>
-        <CharacterCountdown maxLength={140} />
+    <div className={scss.mainContainer}>
+      <div className={scss.gridContainer}>
+        <div className={scss.gridSwitches} />
+        <div className={scss.gridInput} />
+        <div className={scss.gridTabs} />
       </div>
     </div>
   );
