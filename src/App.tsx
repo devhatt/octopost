c2d07ebe6896301892c44d78a401d931c0092ded
Luigi Nicoletti <callmenicoletti@gmail.com>
2023-08-10 16:09:48 -0300

    feat/#28 (#41)
    * feat: create a grid layout for home page
    
    * feat: create a home page grid layout
    
    * fix: fix some issues to pull request review
    
    * fix: fix last issue
    
    * fix: fix one more issue to pullrequest review

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react';
-
-import Testinho from '~components/Testinho/Testinho';
-
 import scss from './App.module.scss';
 
 import './styles/base.scss';
 
-interface ITest {
-  sim: string;
-}
-
-function App(role: ITest) {
-  const [sim, setSim] = useState('');
-
-  const ali = (opa: string) => {
-    setSim(opa);
-  };
-
+function App() {
   return (
-    <div className={scss.teste} id="asdasdas" onClick={() => ali('oi')}>
-      <p>Hello World! SIM?</p> {role.sim} {sim}
-      <Testinho />
+    <div className={scss.mainContainer}>
+      <div className={scss.gridContainer}>
+        <div className={scss.gridSwitches} />
+        <div className={scss.gridInput} />
+        <div className={scss.gridTabs} />
+      </div>
     </div>
   );
 }
