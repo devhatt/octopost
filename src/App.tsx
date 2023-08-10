d6f5d88b81185e07f545daf12b7c9571c27f6f86
Nicoletti <callmenicoletti@gmail.com>
2023-08-10 19:47:21 -0300

    fix: last issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/home';
 
-import scss from './App.module.scss';
+import scss from './app.module.scss';
 
 import './styles/base.scss';
 
 function App() {
   return (
-    <div className={scss.App}>
+    <div className={scss.app}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
