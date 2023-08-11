18c2f0ebde928862aa8a20d25f01b8d308a114a7
flavia <flaviafreirec@hotmail.com>
2023-08-11 16:28:51 -0300

    feat: add landle and button on testinho
    feat #63

---
+++ src/components/Testinho/Testinho.stories.tsx
@@ -0,0 +1,5 @@
+import type { Story } from '@ladle/react';
+
+import Testinho from './Testinho';
+
+export const World: Story = () => <Testinho />;
--- src/components/Testinho/Testinho.tsx
+++ src/components/Testinho/Testinho.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Testinho = ({ name = 'World' }) => {
-  return <h1>Hello, {name}!</h1>;
+  const [clicked, setClicked] = useState(false);
+  const [newName, setNewName] = useState(name);
+
+  const handleButtonClick = () => {
+    if (!clicked) {
+      setNewName(name);
+    } else {
+      setNewName('clicou');
+    }
+    setClicked(!clicked);
+  };
+
+  const Button = () => <button onClick={handleButtonClick}>Click Me</button>;
+
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <h1>{newName}</h1>
+      <Button />
+    </div>
+  );
 };
 
 export default Testinho;
