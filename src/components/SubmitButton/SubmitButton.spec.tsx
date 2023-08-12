b3572958d080bc3597878a67a55a439cd3cc3aa9
jotape <jp.burgarelli@gmail.com>
2023-08-12 11:42:14 -0300

    feat: add Submit Button

---
+++ src/components/SubmitButton/SubmitButton.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import SubmitButton from './SubmitButton';
+
+describe('Submit Button component', () => {
+  test('render Submit Button component', () => {
+    render(<SubmitButton />);
+  });
+});
