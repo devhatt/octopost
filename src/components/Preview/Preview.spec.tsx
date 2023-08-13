13a7f25b2ff059b9452fadfa42d9859f93599abd
hxsggsz <vt.hugo.2021@gmail.com>
2023-08-13 16:23:54 -0300

    test: remove shoud prefix on test
    test #59

--- src/components/Preview/Preview.spec.tsx
+++ src/components/Preview/Preview.spec.tsx
@@ -4,7 +4,7 @@ import Preview from './Preview';
 
 describe('Preview', () => {
   describe('when pass the preview props', () => {
-    it('should render on screen the preview', () => {
+    it('renders on screen the preview', () => {
       render(<Preview previewText="test" />);
       const preview = screen.getByText(/test/i);
       expect(preview).toBeInTheDocument();
