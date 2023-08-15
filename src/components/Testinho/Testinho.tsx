62a65fbd25e01e263395e0830d41860a7f3c25bf
Flávia Freire <54037222+flaviafreirec@users.noreply.github.com>
2023-08-15 09:00:39 -0300

    feat: add landle and test button #63 (#65)
    * feat: add landle and button on testinho
    
    feat #63
    
    * feat: add landle and button on testinho
    
    feat #63
    
    * refactor: edited testinho.stories and ladle dependencie
    
    feat #63
    
    * chore: move devDependencie misplaced as dependencie
    
    * refactor: edit component Testinho and remove useState
    
    ---------
    
    Co-authored-by: Andre Almeida <andre-silva78@hotmail.com>

--- src/components/Testinho/Testinho.tsx
+++ src/components/Testinho/Testinho.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
 const Testinho = ({ name = 'World' }) => {
-  return <h1>Hello, {name}!</h1>;
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+    </div>
+  );
 };
 
 export default Testinho;
