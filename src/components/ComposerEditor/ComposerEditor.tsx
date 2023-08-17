4de427ee7329c30694e07a204ee2d757233d2e3e
Nicoletti <callmenicoletti@gmail.com>
2023-08-17 19:11:34 -0300

    fix: fix the new code reviews on pr

--- src/components/ComposerEditor/ComposerEditor.tsx
+++ src/components/ComposerEditor/ComposerEditor.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
 import CharacterLimit from '~components/CharacterLimit/CharacterLimit';
-import scss from '~components/ComposerEditor/ComposerEditor.module.scss';
 import CustomTextArea from '~components/TextArea/TextArea';
+
+import scss from './ComposerEditor.module.scss';
 
 const ComposerEditor = () => {
   const [inputText, setInputText] = useState('');
