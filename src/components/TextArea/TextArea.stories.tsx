90b423425cff8660bf6818ffa42d576dd5b8e7c4
flavia <flaviafreirec@hotmail.com>
2023-08-18 13:06:45 -0300

    docs: add storie for text area
    docs #102

---
+++ src/components/TextArea/TextArea.stories.tsx
@@ -0,0 +1,9 @@
+import { Story } from '@ladle/react';
+
+import CustomTextArea from './TextArea';
+
+export const TextAreaStories: Story = () => (
+  <div>
+    <CustomTextArea onTextChange={() => {}} />
+  </div>
+);
