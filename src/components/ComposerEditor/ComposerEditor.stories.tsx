070dda06de3df910f6ea62c9bc3fc8926c2799db
Nicoletti <callmenicoletti@gmail.com>
2023-08-23 17:02:52 -0300

    feat: create the ComposerEditor Ladle

---
+++ src/components/ComposerEditor/ComposerEditor.stories.tsx
@@ -0,0 +1,9 @@
+import { Story } from '@ladle/react';
+
+import ComposerEditor from './ComposerEditor';
+
+export const TextAreaStories: Story = () => (
+  <div>
+    <ComposerEditor />
+  </div>
+);
