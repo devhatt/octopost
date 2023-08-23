142c850d9059d860b7ca927425fd3ba99369cf59
flavia <flaviafreirec@hotmail.com>
2023-08-23 13:51:34 -0300

    refactor: adjust home containers and remove borders
    re #114

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,18 +1,26 @@
 import ComposerEditor from '~components/ComposerEditor/ComposerEditor';
 
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+
 import scss from './home.module.scss';
 
 const Home = () => {
   return (
-    <div className={scss.mainContainer}>
-      <div className={scss.gridContainer}>
-        <div className={scss.gridSwitches} />
-        <div className={scss.gridInput}>
-          <ComposerEditor />
+    <>
+      <Header />
+      <div className={scss.mainContainer}>
+        <div className={scss.gridContainer}>
+          <div className={scss.gridSwitches}>
+            <Sidebar />
+          </div>
+          <div className={scss.gridInput}>
+            <ComposerEditor />
+          </div>
+          <div className={scss.gridTabs} />
         </div>
-        <div className={scss.gridTabs} />
       </div>
-    </div>
+    </>
   );
 };
 
