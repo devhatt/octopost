3d9af45f5a13f7eef9e3937ea9c7c50144ce6577
flavia <flaviafreirec@hotmail.com>
2023-08-28 13:38:13 -0300

    docs: add ladle storie for header and sidebar
    docs #114

---
+++ src/pages/home/components/Header/Header.stories.tsx
@@ -0,0 +1,9 @@
+import { Story } from '@ladle/react';
+
+import Header from './Header';
+
+export const HeaderStories: Story = () => (
+  <div>
+    <Header />
+  </div>
+);
---
+++ src/pages/home/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,9 @@
+import { Story } from '@ladle/react';
+
+import Sidebar from './Sidebar';
+
+export const SidebarStories: Story = () => (
+  <div style={{ width: '250px' }}>
+    <Sidebar />
+  </div>
+);
