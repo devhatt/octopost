6d1a4ba7fbf48dc46a01df82f3780bdbe1b39091
Andre Almeida <andre-silva78@hotmail.com>
2023-09-05 15:45:41 -0300

    Create composer base test and fix path alias on jest (#148)
    * fix: update jest config to module alias be resolved for jest
    
    * feat: create composer editor test
    
    * chore: update package json folders aliases

---
+++ src/components/ComposerEditor/ComposeEditor.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+
+import ComposerEditor from './ComposerEditor';
+
+const placeholderText = 'Digite algo aqui...';
+
+describe('ComposeEditor', () => {
+  describe('when render component', () => {
+    it('successfully renders', () => {
+      const { getByPlaceholderText } = render(<ComposerEditor />);
+      const textArea = getByPlaceholderText(placeholderText);
+
+      expect(textArea).toBeInTheDocument();
+    });
+  });
+});
