bba14f660b9ee23203147780fd6fbf9e7e361d7b
flavia <flaviafreirec@hotmail.com>
2023-09-12 16:15:35 -0300

    test: add tests for search clue component
    test #122

--- src/components/SearchClue/SearchClue.tsx
+++ src/components/SearchClue/SearchClue.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 import scss from './SearchClue.module.scss';
 
 import { ISearchClueProps } from './SearchClue.types';
 
 function SearchClue(props: ISearchClueProps) {
-  const { value, label } = props;
+  const { label } = props;
+
+  const [value, setValue] = useState(props.value || '');
+
+  const handleClearClick = () => {
+    setValue('');
+  };
   return (
     <div className={scss.searchClueContainer}>
       <span className={scss.spanSearchClue}>
         {label} <span className={scss.searchWord}>{value}</span>
       </span>
-      <button className={scss.clearButton}>Clean</button>
+      <button className={scss.clearButton} onClick={handleClearClick}>
+        Clean
+      </button>
     </div>
   );
 }
