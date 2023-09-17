c4d1fc4c9f782ee6a08e2a303143c7d24f332412
Leandro Augusto <leandroaugusto470@gmail.com>
2023-09-17 10:43:01 -0300

    feat: feat: added new button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,68 @@
+﻿import classNames from 'classnames';
+
+import scss from '~components/Button/Button.module.scss';
+
+import { ITextButtonProps, ICircleButtonProps } from './Button.types';
+
+function Button(props: ITextButtonProps | ICircleButtonProps) {
+  const RenderTextButton = () => {
+    const {
+      disableElevation,
+      className,
+      children,
+      disabled,
+      onClick,
+      variant,
+      color,
+      type,
+    } = props as ITextButtonProps;
+
+    const classes: string = classNames(
+      className,
+      scss.textButton,
+      scss[`${color ? color : 'primary'}`],
+      scss[`${variant ? variant : 'text'}`],
+      scss[`${disabled ? 'disabled' : ''}`],
+      scss[`${disableElevation ? 'disableElevation' : ''}`]
+    );
+
+    return (
+      <button
+        type={type ? type : 'button'}
+        className={classes}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  };
+
+  const RenderCircleButton = () => {
+    const { className, disabled, variant, color, type, onClick, icon } =
+      props as ICircleButtonProps;
+
+    const classes: string = classNames(
+      className,
+      scss.circleButton,
+      scss[`${color ? color : 'primary'}`],
+      scss[`${variant ? variant : 'text'}`],
+      scss[`${disabled ? 'disabled' : ''}`]
+    );
+
+    return (
+      <button
+        type={type ? type : 'button'}
+        className={classes}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {icon}
+      </button>
+    );
+  };
+
+  return 'circle' in props ? RenderCircleButton() : RenderTextButton();
+}
+
+export default Button;
---
+++ src/components/Button/Button.types.ts
@@ -0,0 +1,17 @@
+﻿import { ReactElement } from 'react';
+
+export interface IButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  variant?: 'text' | 'container' | 'outlined';
+  type?: 'button' | 'submit' | 'reset';
+  color?: 'primary' | 'secondary';
+  disabled?: boolean;
+}
+
+export interface ITextButtonProps extends IButtonProps {
+  disableElevation?: boolean;
+}
+
+export interface ICircleButtonProps extends IButtonProps {
+  icon: ReactElement;
+  circle: boolean;
+}
