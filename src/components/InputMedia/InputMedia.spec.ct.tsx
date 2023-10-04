9605b604217327aada28de3f5172fdb3b98a7a8c
hxsggsz <vt.hugo.2021@gmail.com>
2023-10-04 19:42:46 -0300

    test: add onChange prop for tests
    fix #133

--- src/components/InputMedia/InputMedia.spec.ct.tsx
+++ src/components/InputMedia/InputMedia.spec.ct.tsx
@@ -5,16 +5,22 @@ import InputMedia from './InputMedia';
 test.describe('InputMedia', () => {
   test.describe('when click on input', () => {
     test('changes the image', async ({ mount }) => {
-      const component = await mount(<InputMedia />);
+      let functionCalled = false;
+
+      const component = await mount(
+        <InputMedia onChange={() => (functionCalled = true)} />
+      );
       await component
         .getByTestId('imageInput')
         .setInputFiles('src/assets/logo.png');
+
+      expect(functionCalled).toBeTruthy();
       await expect(component.getByAltText(/selected image/i)).toBeVisible();
     });
 
     test.describe('when the file is diferent from image or video', () => {
       test('doesnt select the file', async ({ mount }) => {
-        const component = await mount(<InputMedia />);
+        const component = await mount(<InputMedia onChange={() => {}} />);
         await component
           .getByTestId('imageInput')
           .setInputFiles('public/robots.txt');
