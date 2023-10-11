dbbfc77fe91473f4a9a83035d10ff4fca01c2b38
flavia <flaviafreirec@hotmail.com>
2023-10-11 17:55:15 -0300

    test: add tests for manyinputs components

---
+++ src/components/ManyInputMedias/ManyInputs.mock.tsx
@@ -0,0 +1,6 @@
+import ManyMediaInputs from './ManyInputs';
+
+// https://github.com/microsoft/playwright/issues/27439
+export function InputMediaForTest() {
+  return <ManyMediaInputs />;
+}
---
+++ src/components/ManyInputMedias/ManyInputs.spec.ct.tsx
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+
+import ManyMediaInputs from './ManyInputs';
+
+test.describe('ManyInputs', () => {
+  test.describe('when click on input', () => {
+    test('upload the image', async ({ mount }) => {
+      const component = await mount(<ManyMediaInputs />);
+
+      await component
+        .getByTestId('imageInput')
+        .setInputFiles('src/assets/logo.png');
+
+      await expect(component.getByTestId('image logo.png')).toBeVisible();
+    });
+
+    test('upload two images', async ({ mount }) => {
+      const component = await mount(<ManyMediaInputs />);
+
+      await component
+        .getByTestId('imageInput')
+        .setInputFiles(['src/assets/logo.png', 'src/assets/imagetest.jpg']);
+
+      await expect(component.getByTestId('image logo.png')).toBeVisible();
+      await expect(component.getByTestId('image imagetest.jpg')).toBeVisible();
+    });
+
+    test.describe('when the file is diferent from image or video', () => {
+      test('doesnt select the file', async ({ mount }) => {
+        const mediaSelected: string | null = null;
+
+        const component = await mount(<ManyMediaInputs />);
+        await component
+          .getByTestId('imageInput')
+          .setInputFiles('public/robots.txt');
+        await expect(mediaSelected).toBeNull();
+      });
+    });
+
+    test.describe('when add img and click on remove button', () => {
+      test('remove image', async ({ mount }) => {
+        const component = await mount(<ManyMediaInputs />);
+
+        await component
+          .getByTestId('imageInput')
+          .setInputFiles('src/assets/logo.png');
+
+        await expect(component.getByTestId('image logo.png')).toBeVisible();
+
+        await component.getByRole('button', { name: 'X' }).click();
+        await expect(component.getByTestId('image logo.png')).not.toBeVisible();
+      });
+    });
+  });
+});
