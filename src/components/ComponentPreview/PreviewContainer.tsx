27e05f591bb99fdcf428d8e99d8fadd9fdf4d30f
Paulo Victor Torres Silva <pilutechinformatica@gmail.com>
2023-11-02 10:09:45 -0300

    refactor: Change component name
    re #178

--- src/components/ComponentPreview/PreviewContainer.tsx
+++ src/components/ComponentPreview/PreviewContainer.tsx
@@ -2,14 +2,8 @@ import scss from './PreviewContainer.module.scss';
 
 import { IComponentPreviewProps } from './PreviewContainer.types';
 
-const ComponentPreview = (props: IComponentPreviewProps) => {
-  return (
-    <section className={scss.container}>
-      {/* Remover H1 quando inserir o componente view */}
-      <h1>Elemento a ser renderizado</h1>
-      {props.preview}
-    </section>
-  );
+const PreviewContainer = (props: IComponentPreviewProps) => {
+  return <section className={scss.container}>{props.preview}</section>;
 };
 
-export default ComponentPreview;
+export default PreviewContainer;
