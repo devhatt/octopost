7ca64c83d3283b4f487ffaa73b306bbe35cac070
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-21 23:46:09 -0300

    test: add AccordionTab tests

---
+++ src/components/AccordionTab/AccordionTab.spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+
+import AccordionTab from './AccordionTab';
+
+import type { TAccordionTabProps } from './AccordionTab.types';
+
+const makeSut = (props: Partial<TAccordionTabProps>) => {
+  return render(<AccordionTab {...props} />);
+};
+
+describe('AccordionTab', () => {
+  describe('when [hideCloseButton] is false', () => {
+    it('not render close button', () => {
+      makeSut({});
+
+      const button = screen.getByRole('button');
+
+      expect(button);
+    });
+  });
+
+  describe('when [renderHeader] is passed', () => {
+    it('render custom header', () => {
+      makeSut({
+        renderHeader: () => <header>header</header>,
+      });
+
+      const header = screen.getByText('header');
+
+      expect(header);
+    });
+
+    it('should function be called with children', () => {
+      makeSut({
+        children: 'children',
+        renderHeader: ({ children }) => <header>{children}</header>,
+      });
+
+      const children = screen.getByText('children');
+
+      expect(children);
+    });
+  });
+});
