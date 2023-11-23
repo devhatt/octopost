b3736d5299d6b629cfd226f0e5f01033f14fd461
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-23 15:40:01 -0300

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,8 @@ module.exports = defineConfig({
     'node_modules',
     '!*.scss',
   ],
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'eslint:recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  plugins: ['prettier', '@typescript-eslint', 'eslint-plugin-import-helpers'],
+  extends: ['eslint:recommended'],
+  plugins: ['prettier', 'eslint-plugin-import-helpers'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'error',
@@ -52,45 +47,66 @@ module.exports = defineConfig({
         alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
-
-    //  TODO: Colocar regra de underscore em variaveis membro e regra de nomeclatura de classe abstrata
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'interface',
-        prefix: ['I'],
-        format: ['PascalCase'],
-      },
-      {
-        selector: 'enum',
-        prefix: ['E'],
-        format: ['PascalCase'],
-      },
-      {
-        selector: 'typeAlias',
-        prefix: ['T'],
-        format: ['PascalCase'],
-      },
-    ],
-
-    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   overrides: [
+    // test files with vitest
+    {
+      files: ['**/*.spec.*'],
+      extends: ['plugin:vitest/recommended'],
+      plugins: ['vitest'],
+    },
+    // react rules
+    {
+      files: ['src/**.(ts|tsx)'],
+      extends: ['plugin:react-hooks/recommended', 'plugin:react/recommended'],
+      plugins: ['react'],
+    },
+
+    // stories
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+    // configuration files
     {
       files: '.eslintrc.js',
       rules: {
         'no-undef': 'off',
       },
     },
+    // validate typescript files
     {
-      files: 'vite.config.ts',
-      extends: ['plugin:@typescript-eslint/recommended-type-checked'],
-      parserOptions: {
-        project: ['./tsconfig.node.json'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        //  TODO: Colocar regra de underscore em variaveis membro e regra de nomeclatura de classe abstrata
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'interface',
+            prefix: ['I'],
+            format: ['PascalCase'],
+          },
+          {
+            selector: 'enum',
+            prefix: ['E'],
+            format: ['PascalCase'],
+          },
+          {
+            selector: 'typeAlias',
+            prefix: ['T'],
+            format: ['PascalCase'],
+          },
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
     {
-      files: ['./src/types/*.ts'],
+      files: ['./src/types/*.ts', '*.d.ts'],
       rules: {
         '@typescript-eslint/no-explicit-any': 'off',
       },
@@ -100,12 +116,6 @@ module.exports = defineConfig({
       rules: {
         'no-console': 'off',
         '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-    {
-      files: ['**/*.stories.tsx'],
-      rules: {
-        'no-console': 'off',
       },
     },
   ],
