3a615ed5d4ef95b2e95f17b64874271e963cdb77
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-23 08:49:42 -0300

    fix: change browser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/home';
 
