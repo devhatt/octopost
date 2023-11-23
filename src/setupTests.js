2d7e50359d8e9ed300084a9cec29774530550f3b
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-23 15:41:06 -0300

    chore: update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,12 @@
-import { afterEach } from 'vitest';
+// add Vitest functions here globally
+import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import matchers from '@testing-library/jest-dom/matchers';
 
-// runs a cleanup after each test case (e.g. clearing jsdom)
+// Extend Vitest's expect method with methods from react-testing-library
+expect.extend(matchers);
+
+// Run cleanup after each test case (e.g., clearing jsdom)
 afterEach(() => {
   cleanup();
 });
