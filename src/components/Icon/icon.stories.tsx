090dd79cbe9131481280ae29e0eeb45fa58af69e
Hewerton <hewertonlourenco@gmail.com>
2023-11-24 09:13:38 -0300

    Fix: add the font family icomoon on icon story

--- src/components/Icon/icon.stories.tsx
+++ src/components/Icon/icon.stories.tsx
@@ -3,7 +3,7 @@ import { Story } from '@ladle/react';
 import Icon from './Icon';
 
 export const iconFont: Story = () => (
-  <div>
+  <div style={{ fontFamily: 'Icomoon' }}>
     <Icon icon="at" color="active" size="large" />
   </div>
 );
