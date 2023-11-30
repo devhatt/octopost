997b8aacde8ed452808de92fb518fe449ca04e2e
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-30 16:24:26 -0300

    feat: update MainComposer to use new AccordionTab

--- src/components/MainComposer/MainComposer.tsx
+++ src/components/MainComposer/MainComposer.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import classNames from 'classnames';
-
 import AccordionTab from '~components/AccordionTab/AccordionTab';
 import ComposerEditor from '~components/ComposerEditor/ComposerEditor';
-import '../../i18n';
+import '~i18n';
 
 import scss from './MainComposer.module.scss';
 
@@ -14,16 +12,7 @@ function MainComposer() {
   const { t } = useTranslation();
 
   return (
-    <AccordionTab
-      renderHeader={(props) => (
-        <div className={classNames(props.className, scss.header)}>
-          {props.children}
-        </div>
-      )}
-      isOpen={isOpen}
-      onChangeOpen={setOpen}
-      title="Main Content"
-    >
+    <AccordionTab isOpen={isOpen} onChangeOpen={setOpen} title="Main Content">
       <div className={scss.content}>
         <div className={scss.textInput}>
           <ComposerEditor />
