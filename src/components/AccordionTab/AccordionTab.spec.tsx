c5ad3c1d5be419bce446837abf1335d28079463a
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-11-30 19:26:09 -0300

    refactor: update accordionTab tests

--- src/components/AccordionTab/AccordionTab.spec.tsx
+++ src/components/AccordionTab/AccordionTab.spec.tsx
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react';
 
 import AccordionTab from './AccordionTab';
 
-import type { TAccordionTabProps } from './AccordionTab.types';
-
 beforeEach(() => {
   window.scrollTo = jest.fn();
 });
@@ -12,41 +10,14 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-const makeSut = (props: Partial<TAccordionTabProps>) => {
-  return render(<AccordionTab {...props} />);
-};
-
 describe('AccordionTab', () => {
   describe('when [hideCloseButton] is false', () => {
     it('not render close button', () => {
-      makeSut({});
+      render(<AccordionTab />);
 
       const button = screen.getByRole('button');
 
-      expect(button);
-    });
-  });
-
-  describe('when [renderHeader] is passed', () => {
-    it('render custom header', () => {
-      makeSut({
-        renderHeader: () => <header>header</header>,
-      });
-
-      const header = screen.getByText('header');
-
-      expect(header);
-    });
-
-    it('should function be called with children', () => {
-      makeSut({
-        children: 'children',
-        renderHeader: ({ children }) => <header>{children}</header>,
-      });
-
-      const children = screen.getByText('children');
-
-      expect(children);
+      expect(button).toBeInTheDocument();
     });
   });
 });
