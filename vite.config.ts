726cda5c2947fc1a4c5c93da645a2a0864529731
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-12-01 20:47:13 -0300

    fix: vitest wrong  configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import electron from 'vite-plugin-electron/simple';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import type { InlineConfig } from 'vitest';
-
-declare module 'vite' {
-  export interface UserConfig {
-    test: InlineConfig;
-  }
-}
 
 export default defineConfig({
   plugins: [
@@ -24,17 +17,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '~styles/global.scss': path.join(__dirname, 'src/styles/global.scss'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/setupTests.js'],
-    include: ['src/**/*.spec.ts', 'src/**/*.spec.tsx'],
-    css: {
-      modules: {
-        classNameStrategy: 'non-scoped',
-      },
     },
   },
 });
