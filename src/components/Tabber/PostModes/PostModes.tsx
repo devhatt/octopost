5ac8c04e8a138771714e5c29ecddae1e95eeaa70
Leandro Augusto <leandroaugusto470@gmail.com>
2023-12-06 12:06:54 -0300

    refactor: Change some maps to clean the code

--- src/components/Tabber/PostModes/PostModes.tsx
+++ src/components/Tabber/PostModes/PostModes.tsx
@@ -5,22 +5,21 @@ import scss from '~components/Tabber/PostModes/PostModes.module.scss';
 import { IPostModesProps } from './PostModes.types';
 
 function PostModes(props: IPostModesProps) {
-  return (
-    <div className={scss.postModesHeader}>
-      {props.postModes.map((postMode, index) => (
-        <span
-          key={index}
-          className={classNames(
-            scss.postModeTitle,
-            props.currentPostMode === index && scss.active
-          )}
-          onClick={() => props.handleCurrentPostMode(index)}
-        >
-          {postMode.name}
-        </span>
-      ))}
-    </div>
-  );
+  const RenderPostModes = () =>
+    props.postModes.map((postMode, index) => (
+      <span
+        key={index}
+        className={classNames(
+          scss.postModeTitle,
+          props.currentPostMode === index && scss.active
+        )}
+        onClick={() => props.handleCurrentPostMode(index)}
+      >
+        {postMode.name}
+      </span>
+    ));
+
+  return <div className={scss.postModesHeader}>{RenderPostModes()}</div>;
 }
 
 export default PostModes;
--- src/components/Tabber/Tabs/Tabs.tsx
+++ src/components/Tabber/Tabs/Tabs.tsx
@@ -6,23 +6,22 @@ import scss from '~components/Tabber/Tabs/Tabs.module.scss';
 import { ITabsProps } from './Tabs.types';
 
 function Tabs(props: ITabsProps) {
-  return (
-    <div className={scss.tabsContainer}>
-      {props.socialNetworks.map((socialNetwork) => (
-        <div
-          key={nanoid()}
-          onClick={() => props.handleCurrentTab(socialNetwork)}
-          className={classNames(
-            scss.tab,
-            socialNetwork.id === props.currentTab.id && scss.active
-          )}
-        >
-          {socialNetwork.icon}
-          <span className={scss.tabTitle}>{socialNetwork.name}</span>
-        </div>
-      ))}
-    </div>
-  );
+  const RenderTabs = () =>
+    props.socialNetworks.map((socialNetwork) => (
+      <div
+        key={nanoid()}
+        onClick={() => props.handleCurrentTab(socialNetwork)}
+        className={classNames(
+          scss.tab,
+          socialNetwork.id === props.currentTab.id && scss.active
+        )}
+      >
+        {socialNetwork.icon}
+        <span className={scss.tabTitle}>{socialNetwork.name}</span>
+      </div>
+    ));
+
+  return <div className={scss.tabsContainer}>{RenderTabs()}</div>;
 }
 
 export default Tabs;
