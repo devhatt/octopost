6be819ec4e246278295cc27bb4531f9621fc3bfb
Luigi Nicoletti <nicoletti@Nicolettis-MacBook-Air.local>
2023-12-07 11:19:48 -0300

    fixed some issues but, have no fatal issues yet

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,8 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
