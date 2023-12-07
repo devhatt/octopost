7873769a33fb386bf8ed2b87e8971bb663feb7b4
Luigi Nicoletti <nicoletti@Nicolettis-MacBook-Air.local>
2023-12-07 10:03:57 -0300

    remove declare module.scss

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,8 +1,3 @@
-declare module '*.scss' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
