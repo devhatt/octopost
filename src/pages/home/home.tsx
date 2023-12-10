e4f3fefaeed62c0a6afbf4460e48207735470bde
Leandro Augusto <leandroaugusto470@gmail.com>
2023-12-10 13:14:10 -0300

    perf: Remove Tabber from home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,6 @@ import FirstComment from '~components/FirstComment/FirstComment';
 import MainComposer from '~components/MainComposer/MainComposer';
 import MediaInputs from '~components/MediaInputs/MediaInput';
 import SavBar from '~components/SavBar/SavBar';
-import Tabber from '~components/Tabber/Tabber';
 
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -21,7 +20,6 @@ const Home = () => {
           <div className={scss.gridInput}>
             <MainComposer />
             <MediaInputs />
-            <Tabber />
             <FirstComment />
           </div>
           <div className={scss.gridTabs} />
