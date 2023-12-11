0de5cf6bfc0d5cfc45da3b224722f2feee9deb73
hxsggsz <vt.hugo.2021@gmail.com>
2023-12-11 11:21:10 -0300

    refactor: add error icon on icon component

--- src/components/Icon/icon.types.ts
+++ src/components/Icon/icon.types.ts
@@ -1,5 +1,5 @@
 export interface IIconProps {
-  icon: 'at' | 'emote' | 'hashtag' | 'link' | 'pin' | 'gpt' | 'canva';
+  icon: 'at' | 'emote' | 'hashtag' | 'link' | 'pin' | 'gpt' | 'canva' | 'error';
   size?: 'small' | 'large';
   color?: 'active' | 'disabled';
   className?: string;
