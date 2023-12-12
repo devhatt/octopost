1a726fdbe62dedc88c4387a715518dd1a2530bfe
Leandro Augusto <61525671+leandrokzu@users.noreply.github.com>
2023-12-12 17:38:46 -0300

    Tabber/#34 (#252)
    * feat: create Tabber Component
    
    * refactor: Change to reduce complexity of Tabber Component
    
    * refactor: Remove Tabber from home
    
    * refactor: Change some maps to clean the code
    
    * refactor: Change to a cleaner and more readable code
    
    * perf: Remove Tabber from home
    
    * refactor: Change to a more consistent code

---
+++ src/components/Tabber/stores/useSocialNetworkStore.types.ts
@@ -0,0 +1,7 @@
+﻿import { ExampleModule1 } from 'modules/example-module1/example-module1';
+
+export type TSocialNetworks = Omit<ExampleModule1, 'services'> & { id: string };
+
+export type TSocialNetworksState = {
+  socialNetworks: TSocialNetworks[];
+};
