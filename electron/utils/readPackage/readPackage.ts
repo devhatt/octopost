ba73fb2332c3bf57b6206d375057911bfbc55de0
hxsggsz <vt.hugo.2021@gmail.com>
2023-12-19 15:29:34 -0300

    refactor: add jsdocs to document the function to get the content inside package.json

--- electron/utils/readPackage/readPackage.ts
+++ electron/utils/readPackage/readPackage.ts
@@ -2,6 +2,11 @@ import fs from 'node:fs/promises';
 
 import { IPluginMetadata } from './readPackage.types';
 
+/**
+ * get **all** content inside the `package.json` on specified path
+ * @param {string} moduleDir path to find the `package.json`
+ * @returns the content inside `package.json`
+ */
 async function readPackage(moduleDir: string): Promise<IPluginMetadata> {
   const parsedPackage = JSON.parse(
     await fs.readFile(`${moduleDir}/package.json`, 'utf8')
