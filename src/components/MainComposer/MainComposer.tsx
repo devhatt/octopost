3446e31ab3d17387618d10f7458202c853c7e441
jotape <jp.burgarelli@gmail.com>
2023-12-19 16:05:47 -0300

    chore: add path alias and changeset, delete unnecessary things

--- src/components/MainComposer/MainComposer.tsx
+++ src/components/MainComposer/MainComposer.tsx
@@ -19,25 +19,12 @@ function MainComposer() {
           <ComposerEditor />
         </div>
         <div className={scss.contentBot}>
-          <div className={scss.contentBotTop}>
-            <div>left</div>
-            <div className={scss.socialMedias}>
-              <p>Right</p>
-            </div>
-          </div>
+          <div className={scss.contentBotTop}></div>
           <div className={scss.contentBotBot}>
             <div className={scss.mainComposerInputMedia}>
               <MediaInputs />
             </div>
-            {/* <h2>content-bot-bot</h2> */}
-            <div className={scss.iconPulsSave}>
-              <div className={scss.icons}>
-                <p>left</p>
-              </div>
-              <div>
-                <p>right</p>
-              </div>
-            </div>
+            <div className={scss.iconPulsSave}></div>
 
             <p>{t('We have a lot of work')}</p>
           </div>
