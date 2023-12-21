14af994ed30bef685769f7ba1e7e30d82d3d658b
Victor Hugo <vt.hugo.2021@gmail.com>
2023-12-21 11:39:41 -0300

    get metadata inside all modules (#290)
    * refactor: create function to get package.json content
    
    * feat: create function to get all metadata inside packages folder
    
    * refactor: add jsdocs to document the metadata function
    
    * refactor: add jsdocs to document the function to get the content inside package.json
    
    * refactor: translate comment to english
    
    * refactor: changes the metadata object
    
    ---------
    
    Co-authored-by: Andre Almeida <andre-silva78@hotmail.com>

--- electron/utils/resolveModulePath.ts
+++ electron/utils/resolveModulePath.ts
@@ -1,22 +1,16 @@
-import fs from 'node:fs/promises';
 import path from 'path';
+
+import readPackage from './readPackageJson/readPackageJson';
 
 /**
  * function to return the content inside the `main` line inside the `package.json`
  * @param {string} moduleDir  - path to find the package.json without the word `package.json`
+ * @returns the path to main script on `package.json`
  */
-async function resolveModulePath(
-  moduleDir: string
-): Promise<string | undefined> {
-  const parsedPackage = JSON.parse(
-    await fs.readFile(`${moduleDir}/package.json`, 'utf8')
-  );
+async function resolveModulePath(moduleDir: string): Promise<string> {
+  const parsedPackage = await readPackage(moduleDir);
 
   const mainContent = parsedPackage.main;
-
-  if (!mainContent) {
-    throw new Error('conteúdo dentro do package.json não-encontrado');
-  }
 
   return path.join(moduleDir, mainContent);
 }
