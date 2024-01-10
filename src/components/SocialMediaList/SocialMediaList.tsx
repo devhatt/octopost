d3cfb6a2821ca084f6d4511d35aa898913cd5ef4
jotape <jp.burgarelli@gmail.com>
2024-01-10 17:27:37 -0300

    chore: add state

--- src/components/SocialMediaList/SocialMediaList.tsx
+++ src/components/SocialMediaList/SocialMediaList.tsx
@@ -6,6 +6,7 @@ import { ISocialMediaList } from './SocialMediaList.type';
 
 function SocialMediaList(props: ISocialMediaList) {
   const [tags, setTags] = useState<string[]>([]);
+  const [selectedOption, setSelectedOption] = useState('');
 
   const addTag = (newTag: string) => {
     if (!tags.includes(newTag)) {
