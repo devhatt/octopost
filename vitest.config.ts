44b8a4d7c431ac893c8fce6a39d584ffaf75ccce
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-01-14 00:25:17 -0300

    chore: improve test pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,16 +7,22 @@ export default mergeConfig(
   defineConfig({
     server: { open: false },
     test: {
-      globals: true,
-      environment: 'jsdom',
       coverage: {
+        branches: 0,
+        functions: 0,
+        lines: 0,
         provider: 'istanbul',
+        reporter: ['json-summary'],
+        reportOnFailure: true,
+        statements: 0,
       },
-      setupFiles: ['src/setupTests.ts'],
-      include: ['src/**/*.spec.ts', 'src/**/*.spec.tsx'],
       css: {
         modules: { classNameStrategy: 'non-scoped' },
       },
+      environment: 'jsdom',
+      globals: true,
+      include: ['src/**/*.spec.ts', 'src/**/*.spec.tsx'],
+      setupFiles: ['src/setupTests.ts'],
     },
   })
 );
