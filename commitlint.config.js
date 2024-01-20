6a82ab953bbad30f880fa5595eaf654ae463f08b
Paulo Victor <pilutechinformatica@gmail.com>
2024-01-20 00:22:59 -0300

    Configure commitlint and updated prettierignore  (#325)
    * chore: updated branch close #306
    
    * chore: delete pnpm.lock
    
    * chore: add pnpm.lock
    
    jajaja
    
    fix #306
    
    * revert: jsjsjsjs
    
    * chore: add pnpm.lock file
    
    * chore: updated git ignore and configure commitlint
    
    * chore: fix eof
    
    * chore: updated eof
    
    ---------
    
    Co-authored-by: Alexandre Gomes <alecell.dev@gmail.com>
    Co-authored-by: Andre Almeida <andre-silva78@hotmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'always'],
+    'body-empty': [2, 'always'],
+    'footer-empty': [2, 'always'],
   },
 };
