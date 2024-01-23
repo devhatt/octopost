66976b224ba7b0b93b2d8ada58b743725b1f95c8
pedromiguelmvs.dev@gmail.com <pedromiguelmvs>
2024-01-23 11:17:29 -0300

    test: muda o teste de remover imagem para o mediainput

--- src/components/MainComposer/MainComposer.spec.tsx
+++ src/components/MainComposer/MainComposer.spec.tsx
@@ -4,20 +4,6 @@ import userEvent from '@testing-library/user-event';
 import MainComposer from './MainComposer';
 
 describe('MainComposer', () => {
-  // describe('MediaInputs', async () => {
-  //   describe('when add img and click on remove button', () => {
-  //     test('remove image', () => {
-  //       const component = render(<MainComposer isOpen title="Main Content" />);
-  
-  //       screen.getByTestId('removeFile').click();
-  
-  //       expect(
-  //         component.getByAltText('uploaded image logo.png')
-  //       ).not.toBeVisible();
-  //     });
-  //   });
-  // });
-
   describe('when component loads', () => {
     it('MediaInputs must be visible when component starts', async () => {
       render(<MainComposer isOpen title="main content" />);
--- src/components/MediaInputs/MediaInput.spec.tsx
+++ src/components/MediaInputs/MediaInput.spec.tsx
@@ -2,11 +2,9 @@ import { render, screen } from '@testing-library/react';
 
 import MediaInputs from './MediaInput';
 
-vi.mock('nanoid', () => {
-  return {
+vi.mock('nanoid', () => ({
     nanoid: vi.fn(() => 'sua-string-especifica-aqui'),
-  };
-});
+  }));
 
 describe('MediaInputs', () => {
   it('renders the  media inputs', () => {
@@ -15,4 +13,15 @@ describe('MediaInputs', () => {
     const manyMediaComponent = screen.getAllByTestId('manyMediaInputs');
     expect(manyMediaComponent.length).toBeGreaterThan(0);
   });
+
+  // describe('when add img and click on remove button', () => {
+  //   test('remove image', () => {
+  //     render(<MediaInputs />);
+
+  //     screen.getByTestId('removeFile').click();
+
+  //     console.log(result);
+  //     // expect(component).not.toBeVisible();
+  //   });
+  // });
 });
