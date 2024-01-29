73d194b68c95c56c37ad24a5f29ac499c6e71ab0
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-01-29 14:57:04 -0300

    chore: add ladle to lib ui

--- packages/ui/src/components/Example/Example.stories.tsx
+++ packages/ui/src/components/Example/Example.stories.tsx
@@ -1,9 +1,17 @@
-import type { Story } from '@ladle/react';
+import type { Story, StoryDefault } from '@ladle/react';
 
 import { Example } from './Example';
 
 import { ExampleProps } from './Example.types';
 
+export default {
+  title: 'Components/Example',
+} satisfies StoryDefault;
+
 export const ExampleComponent: Story<ExampleProps> = (props) => (
   <Example {...props} />
 );
+
+ExampleComponent.args = {
+  children: 'Hello',
+};
