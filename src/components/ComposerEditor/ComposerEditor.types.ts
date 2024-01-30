c871e7f95f6ae5d45e3939bdee180781f8d5cde8
Paulo Victor T Silva <pilutechinformatica@gmail.com>
2024-01-30 10:18:03 -0300

    chore: test remote

--- src/components/ComposerEditor/ComposerEditor.types.ts
+++ src/components/ComposerEditor/ComposerEditor.types.ts
@@ -5,4 +5,5 @@ export type TInputChange = (newText: string) => void;
 export type TComposerEditorProps = PropsWithChildren<{
   inputText: string;
   onTextChange?: TInputChange;
+  test?: string;
 }>;
