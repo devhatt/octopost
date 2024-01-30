64abc35c28867db0bc80ac51d2a823534b4ba574
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-01-29 21:01:27 -0300

    chore: add exports

--- packages/ui/src/components/Example/Example.stories.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from 'react/jsx-runtime';
-import { Example } from './Example';
-export default {
-  title: 'Components/Example',
-};
-export const ExampleComponent = (props) => _jsx(Example, { ...props });
-ExampleComponent.args = {
-  children: 'Hello',
-};
