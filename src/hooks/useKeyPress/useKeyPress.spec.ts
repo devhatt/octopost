f92a51fbdd781fdfb643c4bd1c822af4619dde18
Leandro Augusto <leandroaugusto470@gmail.com>
2024-02-09 10:01:54 -0300

    feat: add tests for useKeyPress

---
+++ src/hooks/useKeyPress/useKeyPress.spec.ts
@@ -0,0 +1,35 @@
+﻿import { act, renderHook } from '@testing-library/react';
+
+import useKeyPress from './useKeyPress';
+
+describe('useKeyPress', () => {
+  let action: (event: KeyboardEvent) => void;
+  let targetKey: string;
+
+  beforeEach(() => {
+    action = vi.fn();
+    targetKey = 'Enter';
+  });
+
+  test('should call action function when target key is pressed', () => {
+    renderHook(() => useKeyPress(targetKey, action));
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: targetKey });
+      window.dispatchEvent(event);
+    });
+
+    expect(action).toHaveBeenCalled();
+  });
+
+  test('should not call action function when a different key is pressed', () => {
+    renderHook(() => useKeyPress(targetKey, action));
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: 'Escape' });
+      window.dispatchEvent(event);
+    });
+
+    expect(action).not.toHaveBeenCalled();
+  });
+});
