7efe74e80bde40e4aa8e32f01182cd2ee4676c4a
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-02-10 11:10:56 -0300

    chore: saving steps

--- packages/module-manager/src/register.ts
+++
@@ -1,9 +0,0 @@
-class AddonEventTarget extends EventTarget {
-  constructor() {
-    super();
-  }
-
-  public add(addon: any) {
-    this.dispatchEvent(new CustomEvent('addon', { detail: addon }));
-  }
-}
