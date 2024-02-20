bd3bc1f09eecd8234ec7b3f57fe3154d14972a15
Leandro Augusto <61525671+leandrokzu@users.noreply.github.com>
2024-02-20 19:00:43 -0300

    feat: a custom hook to listen to key presses (#391)
    * feat: a custom hook to listen to key presses
    
    * chore: changed useKeyPress to a more aproprietade folder
    
    * feat: add tests for useKeyPress
    
    * refactor: reducing redundancies in code

---
+++ src/hooks/useKeyPress/useKeyPress.spec.ts
@@ -0,0 +1,35 @@
+﻿import { act, renderHook } from '@testing-library/react';
+
+import useKeyPress from './useKeyPress';
+
+describe('useKeyPress', () => {
+  let action: (event: KeyboardEvent) => void;
+  let targetKey: string;
+
+  beforeEach(() => {
+    action = vi.fn();
+    targetKey = 'Enter';
+  });
+
+  it('calls action function when target key is pressed', () => {
+    renderHook(() => useKeyPress(targetKey, action));
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: targetKey });
+      window.dispatchEvent(event);
+    });
+
+    expect(action).toHaveBeenCalled();
+  });
+
+  it('does not call action function when a different key is pressed', () => {
+    renderHook(() => useKeyPress(targetKey, action));
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: 'Escape' });
+      window.dispatchEvent(event);
+    });
+
+    expect(action).not.toHaveBeenCalled();
+  });
+});
