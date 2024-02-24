4f865365e4f1e7622e4102bf6c02571e91189ae4
alecell <alecell.dev@gmail.com>
2024-02-24 19:07:22 -0300

    style: remove all guide comments on playwright-ct config

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -7,11 +7,8 @@ import viteConfig from './vite.config';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -26,30 +23,22 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
   ],
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './__snapshots__',
   testDir: './',
   testMatch: '**/*.ct.spec.tsx',
-  /* Maximum time one test can run for. */
   timeout: 10 * 1000,
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Port to use for Playwright component endpoint. */
     ctPort: 3100,
 
     ctViteConfig: {
       plugins: [tsconfigPaths()],
       resolve: viteConfig.resolve,
     },
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
 });
