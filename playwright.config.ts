d579af6089c44f1a01f6b2168143945a1ea47276
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-02-25 17:18:56 -0300

    chore: fix config files to be ts and remove typescript errors

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,16 +17,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
   reporter: 'html',
   retries: process.env.CI ? 2 : 0,
