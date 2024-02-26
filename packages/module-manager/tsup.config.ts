a71f1e209de8204965673ca89383deb8bf8ef973
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-02-26 19:34:39 -0300

    chore: update module-manager to use vite

--- packages/module-manager/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  clean: true,
-  dts: { entry: ['./src/index.ts'] },
-  entry: ['./src/index.ts'],
-  format: ['cjs', 'esm'],
-  splitting: false,
-});
---
+++ packages/module-manager/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [tsconfigPaths(), dts()],
+});
