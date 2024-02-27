9e7b19123cf067d62bca0be9949fd1f730f1a83b
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-02-27 17:53:10 -0300

    test: add checkbox test

--- src/components/Checkbox/Checkbox.spec.tsx
+++ src/components/Checkbox/Checkbox.spec.tsx
@@ -5,18 +5,22 @@ import { Checkbox } from './Checkbox';
 
 import type { CheckboxProps } from './Checkbox.types';
 
-const makeSut = ({
-  checked = false,
-  onChange = vi.fn<unknown[]>(),
-  ...props
-}: Partial<CheckboxProps>): RenderResult =>
-  render(
-    <Checkbox checked={checked} onChange={onChange} {...props}>
-      checkbox
-    </Checkbox>
-  );
+const makeSut = (props: Partial<CheckboxProps>): RenderResult =>
+  render(<Checkbox {...props}>checkbox</Checkbox>);
 
 describe('Checkbox', () => {
+  describe('when checkbox render', () => {
+    it('should mount input', async () => {
+      makeSut({});
+
+      const input = screen.getByRole('checkbox');
+
+      await userEvent.click(input);
+
+      expect(input).toBeInTheDocument();
+    });
+  });
+
   describe('when checkbox be clicked', () => {
     it('call [onChange]', async () => {
       const onChange = vi.fn();
--- src/components/Checkbox/Checkbox.types.ts
+++ src/components/Checkbox/Checkbox.types.ts
@@ -3,7 +3,7 @@ import { ComponentPropsWithRef } from 'react';
 type CheckboxBaseProps = Omit<ComponentPropsWithRef<'input'>, 'onChange'>;
 
 export type CheckboxProps = CheckboxBaseProps & {
-  checked: boolean;
-  children: string;
+  checked?: boolean;
+  children?: string;
   onChange?: (checked: boolean) => void;
 };
