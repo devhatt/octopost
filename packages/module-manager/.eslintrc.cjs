b14fe0a3b957a88a65c397da869c6a34bc31212f
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-02-27 12:03:42 -0300

    chore: update module-manager eslint rules

--- packages/module-manager/.eslintrc.cjs
+++ packages/module-manager/.eslintrc.cjs
@@ -1,6 +1,20 @@
 const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
-  extends: ['../../.eslintrc.cjs', 'plugin:import/recommended'],
+  extends: ['../../.eslintrc.cjs'],
   ignorePatterns: ['node_modules', 'dist'],
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/naming-convention': 'off',
+    'import-helpers/order-imports': 'off',
+    'import/order': 'warn',
+  },
 });
