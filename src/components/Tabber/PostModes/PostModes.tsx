3a9ab7603cbaa1049b178938a6000b28f1bb60f5
wellington-tinho <sousa_wellington@outlook.com>
2024-02-28 21:09:11 -0300

    feat: going back to the  to its initial state

--- src/components/Tabber/PostModes/PostModes.tsx
+++ src/components/Tabber/PostModes/PostModes.tsx
@@ -1,7 +1,5 @@
-﻿import { ReactNode } from 'react';
-
-import { PostMode } from '@octopost/module-manager';
-import classNames from 'classnames';
+﻿import classNames from 'classnames';
+import { IPostMode } from 'modules/types';
 
 import { buildPostModeId } from '../utils';
 
@@ -9,34 +7,33 @@ import scss from './PostModes.module.scss';
 
 import { IPostModesProps } from './PostModes.types';
 
-function PostModes(props: IPostModesProps): ReactNode {
-  const postModeClasses = (index: number): string =>
+function PostModes(props: IPostModesProps) {
+  const postModeClasses = (index: number) =>
     classNames({
+      [scss.postModeTitle]: true,
       [scss.active]:
         props.currentPostModeId === buildPostModeId(props.currentTab, index),
-      [scss.postModeTitle]: true,
     });
 
-  const renderPostMode = (postMode: PostMode, index: number): ReactNode => {
+  const renderPostMode = (postMode: IPostMode, index: number) => {
     const postModeId = buildPostModeId(props.currentTab, index);
 
     return (
-      <button
-        className={postModeClasses(index)}
+      <span
         key={postModeId}
+        className={postModeClasses(index)}
         onClick={() => props.onChangePostMode(postMode, postModeId)}
-        type="button"
       >
         {postMode.name}
-      </button>
+      </span>
     );
   };
 
-  const listPostModes: ReactNode = props.currentTab.postModes.map(
-    (element: PostMode, index: number) => renderPostMode(element, index)
+  return (
+    <div className={scss.postModesHeader}>
+      {props.currentTab.postModes.map(renderPostMode)}
+    </div>
   );
-
-  return <div className={scss.postModesHeader}>{listPostModes}</div>;
 }
 
 export default PostModes;
