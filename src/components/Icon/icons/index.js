560577794e784633f4aff8d19fd31ef926028eb0
Luiginicoletti <callmenicoletti@gmail.com>
2024-03-01 10:23:28 -0300

    fix: ci/cd

--- src/components/Icon/icons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Check } from './check.svg?react';
-export { default as LeftArrow } from './left-arrow.svg?react';
-export { default as RightArrow } from './right-arrow.svg?react';
