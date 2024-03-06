537df74644a895c971c144a9575751f87a21fd00
Luiginicoletti <callmenicoletti@gmail.com>
2024-03-06 13:24:24 -0300

    chore: add icon element and remove unused roles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,39 +1,34 @@
-import { ReactPortal, useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
 
+import { Icon } from '~components/Icon/Icon';
+
 import scss from './Modal.module.scss';
 
 import type { TModalProps } from './Modal.types';
 
-function Modal({
-  children,
-  className,
-  footer,
-  isOpen = false,
-  onClickOutside,
-  title,
-}: TModalProps): ReactPortal {
+function Modal(props: TModalProps): ReactNode {
   useEffect(() => {
-    if (isOpen) {
+    if (props.isOpen) {
       document.body.style.overflow = 'hidden';
       return;
     }
 
     document.body.style.overflow = 'auto';
-  }, [isOpen]);
+  }, [props.isOpen]);
 
   return createPortal(
     <AnimatePresence>
-      {isOpen ? (
+      {props.isOpen && (
         <motion.div
           animate={{ opacity: 1 }}
-          className={classNames(scss.container, className)}
+          className={classNames(scss.container, props.className)}
           data-testid="portal"
           exit={{ opacity: 0 }}
-          onClick={onClickOutside}
+          onClick={props.onClickOutside}
           transition={{ duration: 0.3 }}
         >
           <div
@@ -41,18 +36,20 @@ function Modal({
             onClick={(event) => event.stopPropagation()}
           >
             <div className={scss.modalHeader}>
-              <h2>{title}</h2>
+              <h2>{props.title}</h2>
               <div className={scss.headerButtons}>
-                <button onClick={onClickOutside} type="button" />
+                <button onClick={props.onClickOutside} type="button">
+                  <Icon icon="close" size={16} />
+                </button>
               </div>
             </div>
             <div>
-              <div className={scss.modalContentText}>{children}</div>
+              <div className={scss.modalContentText}>{props.children}</div>
             </div>
-            <footer className={scss.modalFooter}>{footer}</footer>
+            <footer className={scss.modalFooter}>{props.footer}</footer>
           </div>
         </motion.div>
-      ) : null}
+      )}
     </AnimatePresence>,
     document.body
   );
