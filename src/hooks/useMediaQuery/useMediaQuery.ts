20eab5b0f81b7c98dcab3c6ad15d88e385677d4f
wellington-tinho <sousa_wellington@outlook.com>
2024-03-07 00:12:41 -0300

    fix: mediaQuery

--- src/hooks/useMediaQuery/useMediaQuery.ts
+++ src/hooks/useMediaQuery/useMediaQuery.ts
@@ -1,11 +1,29 @@
-import { useMedia } from 'react-use';
+import { useEffect, useState } from 'react';
 
 /**
  * Hook to match the media query.
- * @param {string} queryString The media query string. Should be in the format 'media-query: 600px'.
+ * @param {string} queryString The media query string. Should be in the format '600px'.
  * @returns {boolean} Returns true if the media query matches, otherwise false.
  */
 export function useMediaQuery(queryString: string): boolean {
-  const isMatched = useMedia(`(${queryString})`);
-  return isMatched;
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(min-width: ${queryString})`);
+    setMatches(mediaQuery.matches);
+
+    mediaQuery.addEventListener('change', (event) => {
+      setMatches(event.matches);
+    });
+
+    const handleChange = (event: MediaQueryListEvent): void => {
+      setMatches(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, [queryString]);
+
+  return matches;
 }
