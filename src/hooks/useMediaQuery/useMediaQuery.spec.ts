3a5d5007476546e57aacc13919c1d11b561e371f
wellington-tinho <sousa_wellington@outlook.com>
2024-03-14 12:13:17 -0300

    fix: correct mockImplementation in unit test useMediaQuery

--- src/hooks/useMediaQuery/useMediaQuery.spec.ts
+++ src/hooks/useMediaQuery/useMediaQuery.spec.ts
@@ -2,30 +2,36 @@ import { renderHook } from '@testing-library/react';
 
 import { useMediaQuery } from './useMediaQuery';
 
-// window.matchMedia = vi.fn().mockImplementation(query => ({
-//     addEventListener: vi.fn(),
-//     addListener: vi.fn(),
-//     matches: false, // <- red-flag
-//     removeEventListener: vi.fn(),
-//     removeListener: vi.fn()
-//   }));
+window.matchMedia = vi.fn().mockImplementation((query: string) => {
+  const width = window.innerWidth; // Returns the size of the screen
+  const queryValue = query.match(/\d+/g) as RegExpMatchArray; // converts (min-width: 600px) to ['600']
+  const matches = width >= Number(queryValue[0]);
+
+  return {
+    addEventListener: vi.fn(),
+    addListener: vi.fn(),
+    matches: matches,
+    removeEventListener: vi.fn(),
+    removeListener: vi.fn(),
+  };
+});
 
 describe('useMediaQuery', () => {
   it('should return true for matching media query', () => {
-    // Simulate a screen with width greater than 600px
+    // Simulate a screen with width greater than sm
     window.innerWidth = 800;
 
-    const queryString = '600px';
+    const queryString = 'sm';
     const { result } = renderHook(() => useMediaQuery(queryString));
 
     expect(result.current).toBe(true);
   });
 
   it('should return false for non-matching media query', () => {
-    // Simulate a screen with width less than 600px
+    // Simulate a screen with width less than sm
     window.innerWidth = 500;
 
-    const queryString = '600px';
+    const queryString = 'sm';
     const { result } = renderHook(() => useMediaQuery(queryString));
 
     expect(result.current).toBe(false);
