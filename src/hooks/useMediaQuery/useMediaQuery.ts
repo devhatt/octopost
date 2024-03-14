f4d86752abada39a6e8d52371327e7335b2d0307
wellington-tinho <sousa_wellington@outlook.com>
2024-03-14 12:00:39 -0300

    refactor: improve hook

--- src/hooks/useMediaQuery/useMediaQuery.ts
+++ src/hooks/useMediaQuery/useMediaQuery.ts
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react';
 
+// Reference: src/styles/breakpoints.scss
+const breakpoints = {
+  lg: '1240px',
+  md: '905px',
+  sm: '600px',
+  xl: '1440px',
+} as const;
+
+type Breakpoints = keyof typeof breakpoints;
+
 /**
  * Hook to match the media query.
- * @param {string} queryString The media query string. Should be in the format '600px'.
+ * @param {Breakpoints} query The media query string. Should be in the format `sm`, `md`, `lg`, `xl`.
  * @returns {boolean} Returns true if the media query matches, otherwise false.
  */
-export function useMediaQuery(queryString: string): boolean {
+export function useMediaQuery(query: Breakpoints): boolean {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia(`(min-width: ${queryString})`);
+    const mediaQuery = window.matchMedia(`(min-width: ${breakpoints[query]})`);
     setMatches(mediaQuery.matches);
 
     mediaQuery.addEventListener('change', (event) => {
@@ -23,7 +33,7 @@ export function useMediaQuery(queryString: string): boolean {
     mediaQuery.addEventListener('change', handleChange);
 
     return () => mediaQuery.removeEventListener('change', handleChange);
-  }, [queryString]);
+  }, [query]);
 
   return matches;
 }
