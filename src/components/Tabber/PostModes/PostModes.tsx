53931819de0e32d95440d1462e600332b6bdee6c
wellington-tinho <sousa_wellington@outlook.com>
2024-03-19 08:29:45 -0300

    refactor: re-putting types in function returns

--- src/components/Tabber/PostModes/PostModes.tsx
+++ src/components/Tabber/PostModes/PostModes.tsx
@@ -1,5 +1,7 @@
-﻿import classNames from 'classnames';
-import { IPostMode } from 'modules/types';
+﻿import { ReactNode } from 'react';
+
+import { PostMode } from '@octopost/module-manager';
+import classNames from 'classnames';
 
 import { buildPostModeId } from '../utils';
 
@@ -7,21 +9,21 @@ import scss from './PostModes.module.scss';
 
 import { IPostModesProps } from './PostModes.types';
 
-function PostModes(props: IPostModesProps) {
-  const postModeClasses = (index: number) =>
+function PostModes(props: IPostModesProps): ReactNode {
+  const postModeClasses = (index: number): string =>
     classNames({
-      [scss.postModeTitle]: true,
       [scss.active]:
         props.currentPostModeId === buildPostModeId(props.currentTab, index),
+      [scss.postModeTitle]: true,
     });
 
-  const renderPostMode = (postMode: IPostMode, index: number) => {
+  const renderPostMode = (postMode: PostMode, index: number): ReactNode => {
     const postModeId = buildPostModeId(props.currentTab, index);
 
     return (
       <span
-        key={postModeId}
         className={postModeClasses(index)}
+        key={postModeId}
         onClick={() => props.onChangePostMode(postMode, postModeId)}
       >
         {postMode.name}
@@ -31,7 +33,9 @@ function PostModes(props: IPostModesProps) {
 
   return (
     <div className={scss.postModesHeader}>
-      {props.currentTab.postModes.map(renderPostMode)}
+      {props.currentTab.postModes.map((postMode, index) =>
+        renderPostMode(postMode, index)
+      )}
     </div>
   );
 }
