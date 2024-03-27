8b03340404c39f94e1e41f62ee399aa8b073dfbc
Joao Paulo Burgarelli <jp.burgarelli@gmail.com>
2024-03-27 16:19:21 -0300

    chore: add the feedbackerror new test

--- src/components/FeedbackError/FeedbackError.spec.tsx
+++ src/components/FeedbackError/FeedbackError.spec.tsx
@@ -1,17 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import * as useErrorHook from '~stores/useError/useError';
 
 import FeedbackError from './FeedbackError';
 
-describe('FeedbackEror', () => {
-  describe('when initialize', () => {
-    describe('there is no error', () => {
-      it('renders nothing on screen', () => {
-        render(<FeedbackError />);
+describe('FeedbackError', () => {
+  const useErrorSpy = vi.spyOn(useErrorHook, 'useError');
 
-        expect(
-          screen.queryByTestId(/error-container/i)
-        ).not.toBeInTheDocument();
-      });
+  it('When the error array is empty, the component should not be rendered', () => {
+    useErrorSpy.mockReturnValue({
+      errors: [],
     });
+
+    render(<FeedbackError />);
+
+    const feedbackError = screen.queryByTestId('error-container');
+    expect(feedbackError).not.toBeVisible();
+  });
+
+  it('When the error array has at least one error, the component must be rendered', async () => {
+    useErrorSpy.mockReturnValue(['Test error message']);
+
+    render(<FeedbackError />);
+    const dropDownButton = screen.getByRole('button');
+
+    await userEvent.click(dropDownButton);
+
+    const errorEvidence = screen.getByText(/Test error message/);
+    expect(errorEvidence).toBeInTheDocument();
   });
 });
