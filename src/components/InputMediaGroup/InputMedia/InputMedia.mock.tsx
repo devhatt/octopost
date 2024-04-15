6e2165985b5c8dcfa2fe77889150045faa246f92
DominMFD <mfdmatheus@hotmail.com>
2024-04-15 14:06:25 -0300

    fix: remove coment from imput mock

--- src/components/InputMediaGroup/InputMedia/InputMedia.mock.tsx
+++ src/components/InputMediaGroup/InputMedia/InputMedia.mock.tsx
@@ -4,13 +4,6 @@ import InputMedia from './InputMedia';
 
 import { TInputMediaTestWrapper } from './InputMedia.types';
 
-/*
- * Playwright's tests execute both on the server and in a real browser.
- * Due to this constraint, props should be primitive types.
- * This wrapper receives the file name and forwards it to the component for testing purposes.
- * Refer to github.com/microsoft/playwright/issues/27439 for details.
- */
-
 export function InputMediaForTest(props: TInputMediaTestWrapper): ReactNode {
   return (
     <InputMedia
