8113d78624fdfb13bdab5350d47b53076706cdd2
matheus.webp <matheus.webp@gmail.com>
2024-04-20 21:08:36 -0300

    feat: add navbar with octopost logo and social icons to header component

--- src/pages/home/components/Header/Header.tsx
+++ src/pages/home/components/Header/Header.tsx
@@ -1,9 +1,41 @@
+import { ReactNode } from 'react';
+
 import scss from './Header.module.scss';
 
-function Header() {
+import instagramIcon from './assets/instagram.svg';
+import octopostLogo from './assets/octopostLogo.svg';
+import tiktokIcon from './assets/tiktok.svg';
+import twitterIcon from './assets/twitter.svg';
+
+function Header(): ReactNode {
   return (
     <header className={scss.header}>
-      <nav className={scss.nav}>Header</nav>
+      <nav className={scss.nav}>
+        <img alt="octopost logo" src={octopostLogo} />
+        <ul className={scss.navList}>
+          <li className={scss.navItem}>
+            <img
+              alt="tiktok icon"
+              className={scss.navItemImage}
+              src={tiktokIcon}
+            />
+          </li>
+          <li className={scss.navItem}>
+            <img
+              alt="twitter icon"
+              className={scss.navItemImage}
+              src={twitterIcon}
+            />
+          </li>
+          <li className={scss.navItem}>
+            <img
+              alt="instagram icon"
+              className={scss.navItemImage}
+              src={instagramIcon}
+            />
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
