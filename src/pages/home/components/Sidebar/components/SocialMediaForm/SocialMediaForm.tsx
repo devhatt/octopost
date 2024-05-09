c076f102cc4a8f101f5a2a3422b957deebec86cf
Douglas Murita <douglasb008@gmail.com>
2024-05-08 23:03:06 -0300

    feat: add style for social grid

--- src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.tsx
+++ src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.tsx
@@ -1,8 +1,5 @@
 import { ReactNode } from 'react';
 
-import classNames from 'classnames';
-
-import Button from '~components/Button/Button';
 import Modal from '~components/Modal/Modal';
 
 import scss from './SocialMediaList.module.scss';
@@ -12,48 +9,31 @@ import { SocialMediaFormProps } from './SocialMediaForm.types';
 function SocialMediaForm({
   isOpen,
   setIsOpen,
-  ...props
 }: SocialMediaFormProps): ReactNode {
-  // const [selectSocialMedia, setSelectSocialMedia] = useState(
-  //   'Selecione uma rede social'
-  // )
-
   return (
     <Modal
-      footer={
-        <Button disabled={false} type="button" variant="container">
-          any_text
-        </Button>
-      }
       isOpen={isOpen}
       onClickOutside={() => setIsOpen(false)}
       title="selecione sua rede social"
     >
-      <form className={classNames(scss.form)} {...props}>
-        {/* <select
-          className={scss.selection}
-          onChange={(e) => setSelectSocialMedia(e.target.value)}
-          value={selectSocialMedia}
-        >
-          <option className={scss.optionSocial}>{selectSocialMedia}</option>
-          <option className={scss.optionSocial} value="twitter">
-            Twitter
-          </option>
-          <option className={scss.optionSocial} value="instagram">
-            Instagram
-          </option>
-          <option className={scss.optionSocial} value="facebook">
-            Facebook
-          </option>
-        </select> */}
-        <ul className={scss.selection}>
-          <li>Twitter</li>
-          <li>Instagram</li>
-          <li>LinkedIn</li>
-          <li>Youtube</li>
-          <li>Facebook</li>
-        </ul>
-      </form>
+      <ul className={scss.selection}>
+        <li className={scss.socialItem}>
+          <img alt="instagram" src="./social-github.svg" />
+          <p>instagram</p>
+        </li>
+        <li className={scss.socialItem}>
+          <img alt="" src="./social-github.svg" />
+          <p>linkedin</p>
+        </li>
+        <li className={scss.socialItem}>
+          <img alt="" src="./social-github.svg" />
+          <p>twitter</p>
+        </li>
+        <li className={scss.socialItem}>
+          <img alt="" src="./social-github.svg" />
+          <p>facebook</p>
+        </li>
+      </ul>
     </Modal>
   );
 }
