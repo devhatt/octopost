c08bd414c7df9872ae0219e84467670c830038d9
Luigi Nicoletti <nicoletti@Luigis-MacBook-Air.local>
2024-06-05 15:51:30 -0300

    revert: firstComment Component

--- src/components/FirstComment/FirstComment.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story } from '@ladle/react';
-
-import FirstComment from './FirstComment';
-
-import { TFirstCommentProps } from './FirstComment.types';
-
-export const FirstCommentStories: Story<TFirstCommentProps> = (props) => (
-  <FirstComment {...props} />
-);
