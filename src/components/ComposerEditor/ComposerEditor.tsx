07a14bff4f734ef49cf859558945c3645c924a88
Hewerton Zoldy <hewerton.soares@yahoo.com>
2024-06-07 16:15:43 -0300

    refactor: improving reading on socials func

--- src/components/ComposerEditor/ComposerEditor.tsx
+++ src/components/ComposerEditor/ComposerEditor.tsx
@@ -1,40 +1,65 @@
 import { ChangeEvent, ReactNode, useCallback, useState } from 'react';
 
+import {
+  PostMode,
+  TextValidator,
+} from '~services/api/social-media/social-media.types.ts';
 import { useSocialMediaStore } from '~stores/useSocialMediaStore.ts';
 
 import scss from './ComposerEditor.module.scss';
 
 import CharacterLimit from '../CharacterLimitMainText/CharacterLimit.tsx';
-import { Social, TComposerEditorProps } from './ComposerEditor.types';
+import { TComposerEditorProps } from './ComposerEditor.types';
 
 function ComposerEditor(props: TComposerEditorProps): ReactNode {
   const { socialMedias } = useSocialMediaStore();
   const [inputValue, setInputValue] = useState('');
-  const socials = useCallback(() => {
-    let totalMax = 0;
-    let social: Social[] = [];
+
+  const isBigger = useCallback(
+    (
+      currentValidator: PostMode['validators'],
+      currentMax: number
+    ): currentValidator is TextValidator =>
+      'text' in currentValidator &&
+      currentValidator.text.maxLength > currentMax,
+    [socialMedias]
+  );
+
+  //TODO: rename this function
+  const getBiggestPostModeLimitsBySocialMedia = useCallback(() => {
+    const socialLimits = [];
     for (const [, socialMedia] of socialMedias) {
-      const max = socialMedia.postModes.reduce((acc, data): number => {
-        if (!('text' in data.validators)) {
-          return 0;
-        }
-        const { maxLength } = data.validators.text;
-        return acc > maxLength ? acc : maxLength;
-      }, 0);
-      const result = {
-        id: socialMedia.id,
-        maxLength: max,
-        socialName: socialMedia.name,
-        svg: socialMedia.icon,
-      };
-      totalMax = max > totalMax ? max : totalMax;
-      social = [...social, result];
+      //TODO: Create a type of result with {limit: number, socialMediaId: string} | rename result to a better name
+      const result = socialMedia.postModes.reduce<{
+        limit: number;
+        socialMediaId: string;
+      }>(
+        (
+          acc,
+          postMode
+        ): {
+          limit: number;
+          socialMediaId: string;
+        } => {
+          if (isBigger(postMode.validators, acc.limit))
+            acc.limit = postMode.validators.text.maxLength;
+          return acc;
+        },
+        { limit: 0, socialMediaId: socialMedia.id }
+      );
+      socialLimits.push(result);
     }
-    return {
-      social: social,
-      totalMaxLength: totalMax,
-    };
+    return socialLimits;
   }, [socialMedias]);
+  //TODO: rename this function
+  const getBiggestPostModeLimitAndSocials = useCallback(() => {
+    const socialLimits = getBiggestPostModeLimitsBySocialMedia();
+    const maxLimit = socialLimits.reduce(
+      (acc, current) => (acc > current.limit ? acc : current.limit),
+      0
+    );
+    return { maxLimit, socialLimits };
+  }, [getBiggestPostModeLimitsBySocialMedia]);
 
   const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
     const newValue = event.target.value;
@@ -43,7 +68,7 @@ function ComposerEditor(props: TComposerEditorProps): ReactNode {
       props.onChange(newValue);
     }
   };
-
+  const socialLimits = getBiggestPostModeLimitAndSocials();
   return (
     <div className={scss.inputContainer}>
       <textarea
@@ -54,16 +79,16 @@ function ComposerEditor(props: TComposerEditorProps): ReactNode {
       />
       <div className={scss.charactersLimitContainer}>
         <CharacterLimit
-          maxLength={socials().totalMaxLength}
+          maxLength={socialLimits.maxLimit}
           svg={null}
           value={inputValue}
         />
         <div className={scss.characterLimitWrapper}>
-          {socials().social.map((module) => (
+          {socialLimits.socialLimits.map((postMode) => (
             <CharacterLimit
-              key={module.id}
-              maxLength={module.maxLength}
-              svg={module.svg}
+              key={postMode.socialMediaId}
+              maxLength={postMode.limit}
+              svg={socialMedias.get(postMode.socialMediaId)?.icon}
               value={inputValue}
             />
           ))}
