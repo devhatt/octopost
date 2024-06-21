25b6eded73e1acf3fe3fca8a5c598ed6464c1299
Douglas Murita <douglasb008@gmail.com>
2024-06-20 22:40:37 -0300

    refactor: changes in data files for social medias

---
+++ src/pages/home/components/Sidebar/components/SocialMediaForm/data.ts
@@ -0,0 +1,19 @@
+const socialMedias = [
+  {
+    name: 'Facebook',
+  },
+  {
+    name: 'Instagram',
+  },
+  {
+    name: 'Twitter',
+  },
+  {
+    name: 'Tiktok',
+  },
+  {
+    name: 'LinkedIn',
+  },
+];
+
+export { socialMedias };
