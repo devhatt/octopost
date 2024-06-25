7d98b232549f35e27e55433c8309a8cd9aa11473
Douglas Murita <douglasb008@gmail.com>
2024-06-24 22:24:04 -0300

    refactor: change static list to dinymic list

--- src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.tsx
+++ src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.tsx
@@ -2,13 +2,13 @@ import { ReactNode } from 'react';
 
 import Button from '~components/Button/Button';
 
-// import FacebookIcon from './images/facebook.svg?react'
 import scss from './SocialMediaList.module.scss';
 
+import { socialMedias } from './data.ts';
 import FacebookIcon from './images/facebook.svg?react';
-import InstagramIcon from './images/instagram.svg?react';
-import TiktokIcon from './images/tiktok.svg?react';
-import TwitterIcon from './images/twitter.svg?react';
+// import InstagramIcon from './images/instagram.svg?react';
+// import TiktokIcon from './images/tiktok.svg?react';
+// import TwitterIcon from './images/twitter.svg?react';
 import { SocialMediaFormProps } from './SocialMediaForm.types';
 
 function SocialMediaForm({
@@ -16,72 +16,30 @@ function SocialMediaForm({
 }: SocialMediaFormProps): ReactNode {
   return (
     <ul className={scss.selection}>
-      <li className={scss.socialItem}>
-        <button type="button">
-          <FacebookIcon className={scss.socialImage} fill="#000" width={50} />
-          <span className={scss.socialTitle}>Facebook</span>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="outlined"
-          >
-            Connect Account
-          </Button>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="container"
-          >
-            Connect Group
-          </Button>
-        </button>
-      </li>
-      <li className={scss.socialItem}>
-        <button type="button">
-          <TiktokIcon className={scss.socialImage} fill="#000" width={50} />
-          <span className={scss.socialTitle}>TikTok</span>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="outlined"
-          >
-            Connect Account
-          </Button>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="container"
-          >
-            Connect Group
-          </Button>
-        </button>
-      </li>
-      <li className={scss.socialItem}>
-        <button type="button">
-          <TwitterIcon className={scss.socialImage} fill="#000" width={50} />
-          <span className={scss.socialTitle}>Twitter</span>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="container"
-          >
-            Connect Account
-          </Button>
-        </button>
-      </li>
-      <li className={scss.socialItem}>
-        <button type="button">
-          <InstagramIcon className={scss.socialImage} fill="#000" width={50} />
-          <span className={scss.socialTitle}>Instagram</span>
-          <Button
-            className={scss.socialButton}
-            onClick={onHandleToggleModal}
-            variant="outlined"
-          >
-            Connect Account
-          </Button>
-        </button>
-      </li>
+      {socialMedias.map((socialMedia) => (
+        <li className={scss.socialItem} key={socialMedia.id}>
+          <button type="button">
+            <FacebookIcon className={scss.socialImage} fill="#000" width={50} />
+            <span className={scss.socialTitle}>{socialMedia.name}</span>
+            <Button
+              className={scss.socialButton}
+              onClick={onHandleToggleModal}
+              variant="outlined"
+            >
+              Connect Account
+            </Button>
+            {socialMedia.hasGroup && (
+              <Button
+                className={scss.socialButton}
+                onClick={onHandleToggleModal}
+                variant="container"
+              >
+                Connect Group
+              </Button>
+            )}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
--- src/pages/home/components/Sidebar/components/SocialMediaForm/data.ts
+++ src/pages/home/components/Sidebar/components/SocialMediaForm/data.ts
@@ -1,17 +1,33 @@
-const socialMedias = [
+export type socialMediasType = {
+  hasGroup: boolean;
+  id: string;
+  name: string;
+};
+
+const socialMedias: socialMediasType[] = [
   {
+    hasGroup: true,
+    id: crypto.randomUUID(),
     name: 'Facebook',
   },
   {
+    hasGroup: false,
+    id: crypto.randomUUID(),
     name: 'Instagram',
   },
   {
+    hasGroup: false,
+    id: crypto.randomUUID(),
     name: 'Twitter',
   },
   {
+    hasGroup: false,
+    id: crypto.randomUUID(),
     name: 'Tiktok',
   },
   {
+    hasGroup: false,
+    id: crypto.randomUUID(),
     name: 'LinkedIn',
   },
 ];
