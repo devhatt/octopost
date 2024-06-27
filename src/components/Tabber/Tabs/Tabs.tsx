f62771c93529ef09e29e5923054b24eab3adbaa3
juliaam <juliademoraess@gmail.com>
2024-06-27 17:38:02 -0300

    fix: scroll with gsap

--- src/components/Tabber/Tabs/Tabs.tsx
+++ src/components/Tabber/Tabs/Tabs.tsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames';
 
+import { useHorizontalScroll } from '~hooks/useHorizontalScroll/useHorizontalScroll';
 import { Account } from '~services/api/accounts/accounts.types';
 import { useSocialMediaStore } from '~stores/useSocialMediaStore/useSocialMediaStore';
 
@@ -12,6 +13,8 @@ import { TabsProps } from './Tabs.types';
 
 function Tabs(props: TabsProps): ReactNode {
   const { socialMedias } = useSocialMediaStore();
+  const { containerRef, handleMouseEnter, handleMouseLeave, scrollToElement } =
+    useHorizontalScroll();
 
   const tabClasses = (id: TabId): string =>
     classNames(scss.tab, id === props.currentTab.id && scss.active);
@@ -19,11 +22,20 @@ function Tabs(props: TabsProps): ReactNode {
   const renderSocialMediaIcon = (account: Account): ReactNode =>
     socialMedias.get(account.socialMediaId)?.icon;
 
+  const handleTabClick = (
+    tab: Tab,
+    tabId: TabId,
+    tabElement: HTMLElement
+  ): void => {
+    props.onChangeTab(tab, tabId);
+    scrollToElement(tabElement);
+  };
+
   const renderTabs = (tabId: TabId, tab: Tab): ReactNode => (
     <div
       className={tabClasses(tabId)}
       key={tabId}
-      onClick={() => props.onChangeTab(tab, tabId)}
+      onClick={(e) => handleTabClick(tab, tabId, e.currentTarget)}
     >
       {renderSocialMediaIcon(tab.account as Account)}
       <span className={scss.tabTitle}>{tab.account.userName}</span>
@@ -31,7 +43,12 @@ function Tabs(props: TabsProps): ReactNode {
   );
 
   return (
-    <div className={scss.tabsContainer}>
+    <div
+      className={scss.tabsContainer}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      ref={containerRef}
+    >
       {Object.entries(props.tabs).map(([tabId, tab]) =>
         renderTabs(tabId as TabId, tab)
       )}
---
+++ src/hooks/useHorizontalScroll/useHorizontalScroll.ts
@@ -0,0 +1,69 @@
+import { RefObject, useCallback, useRef } from 'react';
+
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/all';
+
+gsap.registerPlugin(ScrollToPlugin);
+
+export const useHorizontalScroll = (): {
+  containerRef: RefObject<HTMLDivElement>;
+  handleMouseEnter: () => void;
+  handleMouseLeave: () => void;
+  scrollToElement: (element: HTMLElement) => void;
+} => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const scrollHorizontally = useCallback((e: WheelEvent) => {
+    e.preventDefault();
+    const scrollAmount = e.deltaY;
+    const container = containerRef.current;
+
+    if (container) {
+      const newScrollPosition = container.scrollLeft + scrollAmount;
+
+      gsap.to(container, {
+        duration: 0.5,
+        ease: 'power3.out',
+        scrollTo: { x: newScrollPosition },
+      });
+    }
+  }, []);
+
+  const handleMouseEnter = useCallback(() => {
+    window.addEventListener('wheel', scrollHorizontally, { passive: false });
+  }, [scrollHorizontally]);
+
+  const handleMouseLeave = useCallback(() => {
+    window.removeEventListener('wheel', scrollHorizontally);
+  }, [scrollHorizontally]);
+
+  const scrollToElement = useCallback((element: HTMLElement) => {
+    const container = containerRef.current;
+
+    if (container) {
+      const containerRect = container.getBoundingClientRect();
+      const elementRect = element.getBoundingClientRect();
+      const isElementVisible =
+        elementRect.left >= containerRect.left &&
+        elementRect.right <= containerRect.right;
+
+      if (!isElementVisible) {
+        const offset = elementRect.left - containerRect.left;
+        const newScrollPosition = container.scrollLeft + offset;
+
+        gsap.to(container, {
+          duration: 0.5,
+          ease: 'power3.out',
+          scrollTo: { x: newScrollPosition },
+        });
+      }
+    }
+  }, []);
+
+  return {
+    containerRef,
+    handleMouseEnter,
+    handleMouseLeave,
+    scrollToElement,
+  };
+};
