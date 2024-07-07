06d04637cc2340576666d12017eee99df6debbc6
juliaam <juliademoraess@gmail.com>
2024-07-07 16:09:22 -0300

    feat: boolean of hasMoreOnLeft or hasMoreOnRight

--- src/hooks/useHorizontalScroll/useHorizontalScroll.ts
+++ src/hooks/useHorizontalScroll/useHorizontalScroll.ts
@@ -1,4 +1,4 @@
-import { RefObject, useCallback, useRef } from 'react';
+import { RefObject, useCallback, useRef, useState } from 'react';
 
 import { gsap } from 'gsap';
 import { ScrollToPlugin } from 'gsap/all';
@@ -11,59 +11,91 @@ export const useHorizontalScroll = (): {
   containerRef: RefObject<HTMLDivElement>;
   handleMouseEnter: () => void;
   handleMouseLeave: () => void;
+  hasMoreOnLeft: boolean;
+  hasMoreOnRight: boolean;
   scrollToElement: (element: HTMLElement) => void;
 } => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [hasMoreOnRight, setHasMoreOnRight] = useState(false);
+  const [hasMoreOnLeft, setHasMoreOnLeft] = useState(false);
 
-  const scrollHorizontally = useCallback((e: WheelEvent) => {
-    e.preventDefault();
-    const scrollAmount = e.deltaY;
+  const updateScrollStatus = useCallback(() => {
     const container = containerRef.current;
 
     if (container) {
-      const newScrollPosition = container.scrollLeft + scrollAmount;
-      gsap.to(container, {
-        duration: 0.5,
-        ease: 'power3.out',
-        scrollTo: { x: newScrollPosition },
-      });
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+      setHasMoreOnLeft(container.scrollLeft > 0);
+      setHasMoreOnRight(container.scrollLeft < maxScrollLeft);
     }
   }, []);
 
+  const scrollHorizontally = useCallback(
+    (e: WheelEvent) => {
+      e.preventDefault();
+      const scrollAmount = e.deltaY;
+      const container = containerRef.current;
+
+      if (container) {
+        const newScrollPosition = container.scrollLeft + scrollAmount;
+        gsap.to(container, {
+          duration: 0.5,
+          ease: 'power3.out',
+          onUpdate: updateScrollStatus,
+          scrollTo: { x: newScrollPosition },
+        });
+      }
+    },
+    [updateScrollStatus]
+  );
+
   const handleMouseEnter = useCallback(() => {
     window.addEventListener('wheel', scrollHorizontally, { passive: false });
-  }, [scrollHorizontally]);
+    updateScrollStatus();
+  }, [scrollHorizontally, updateScrollStatus]);
 
   const handleMouseLeave = useCallback(() => {
     window.removeEventListener('wheel', scrollHorizontally);
   }, [scrollHorizontally]);
 
-  const scrollToElement = useCallback((element: HTMLElement) => {
-    const container = containerRef.current;
+  const scrollToElement = useCallback(
+    (element: HTMLElement) => {
+      const container = containerRef.current;
 
-    if (container) {
-      const containerRect = container.getBoundingClientRect();
-      const elementRect = element.getBoundingClientRect();
+      if (container) {
+        const containerRect = container.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
 
-      const offset = elementRect.left - containerRect.left;
-      const newScrollPosition =
-        container.scrollLeft +
-        offset -
-        container.clientWidth / TWO +
-        elementRect.width / TWO;
+        const isElementVisible =
+          elementRect.left >= containerRect.left &&
+          elementRect.right <= containerRect.right;
 
-      gsap.to(container, {
-        duration: 1,
-        ease: 'power3.out',
-        scrollTo: { x: newScrollPosition },
-      });
-    }
-  }, []);
+        if (!isElementVisible) {
+          const offset = elementRect.left - containerRect.left;
+          const newScrollPosition =
+            container.scrollLeft +
+            offset -
+            container.clientWidth / TWO +
+            elementRect.width / TWO;
+
+          gsap.to(container, {
+            duration: 0.6,
+            ease: 'power3.out',
+            onUpdate: updateScrollStatus,
+            scrollTo: { x: newScrollPosition },
+          });
+        }
+      }
+    },
+    [updateScrollStatus]
+  );
 
   return {
     containerRef,
     handleMouseEnter,
     handleMouseLeave,
+    hasMoreOnLeft,
+    hasMoreOnRight,
     scrollToElement,
   };
 };
