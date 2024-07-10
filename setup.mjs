9c26d50e65f13141d0036ba971f45b759904dace
Paulo Victor T Silva <pilutechinformatica@gmail.com>
2024-07-10 11:29:06 -0300

    fix: ajust prettier erros

--- setup.mjs
+++ setup.mjs
@@ -9,23 +9,23 @@ const __dirname = path.dirname(__filename);
 const envFilePath = path.join(__dirname, '.env');
 
 async function setupDotEnv() {
-    if (fs.existsSync(envFilePath)) {
-        console.log(
-            'O .env já existe, se você deseja recriá-lo, exclua o arquivo e execute este script novamente.'
-        );
-        return;
-    }
+  if (fs.existsSync(envFilePath)) {
+    console.log(
+      'O .env já existe, se você deseja recriá-lo, exclua o arquivo e execute este script novamente.'
+    );
+    return;
+  }
 
-    try {
-        console.log('Baixando o .env do repositório...');
-        const envData = await fetch(
-            'https://raw.githubusercontent.com/devhatt/envs/main/octopost-frontend.env'
-        ).then((response) => response.text());
-        fs.writeFileSync(envFilePath, envData);
-        console.log('O arquivo .env foi criado com sucesso!');
-    } catch (error) {
-        console.error('Erro ao criar o arquivo .env:', error);
-    }
+  try {
+    console.log('Baixando o .env do repositório...');
+    const envData = await fetch(
+      'https://raw.githubusercontent.com/devhatt/envs/main/octopost-frontend.env'
+    ).then((response) => response.text());
+    fs.writeFileSync(envFilePath, envData);
+    console.log('O arquivo .env foi criado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao criar o arquivo .env:', error);
+  }
 }
 
 setupDotEnv();
