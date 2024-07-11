a181a3fcda5d0c6faf98e6c4c1fe7321961321d1
joao burgarelli jp <jp.burgarelli@gmail.com>
2024-07-11 14:08:06 -0300

    chore: add correct icon & adjust name

--- src/components/FeedbackError/FeedbackError.tsx
+++ src/components/FeedbackError/FeedbackError.tsx
@@ -54,7 +54,7 @@ function FeedbackError(): ReactNode {
             <Icon
               className={scss.dropDownIcon}
               data-testid="dropdown-arrow"
-              icon="dropDownArrow"
+              icon="TriangleDropArrow"
               size={10}
             />
           </button>
