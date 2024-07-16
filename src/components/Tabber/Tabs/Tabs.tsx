37d76154b5c5397ef4af191b7adc06fd47cec548
juliaM <35346206+juliaam@users.noreply.github.com>
2024-07-16 12:37:51 -0300

    Tabber scroll - issue 532 (#561)
    * feat: scroll in tabber and remove height and width fix
    
    * feat: scrollbar hidden and fade effect
    
    * fix: gradient color
    
    * fix: increase opacity
    
    * fix: scroll with gsap
    
    * feat: create the base test
    
    * fix: scrollto animation
    
    * fix: remove horizontalscrollhook tests
    
    * fix: consistency of scrollToElement
    
    * feat: boolean of hasMoreOnLeft or hasMoreOnRight
    
    * fix: imports desnececssary
    
    * fix: gradient
    
    * fix: scrolltoelemnt comportament and fix gradient
    
    * fix: remove desnecessary states
    
    * fix: height and alias
    
    ---------
    
    Co-authored-by: Alexandre Gomes <alecell.dev@gmail.com>

--- src/components/Tabber/Tabs/Tabs.tsx
+++ src/components/Tabber/Tabs/Tabs.tsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames';
 
+import { useHorizontalScroll } from '~hooks/useHorizontalScroll/useHorizontalScroll';
 import { Account } from '~services/api/accounts/accounts.types';
 import { useSocialMediaStore } from '~stores/useSocialMediaStore/useSocialMediaStore';
 
@@ -12,6 +13,14 @@ import { TabsProps } from './Tabs.types';
 
 function Tabs(props: TabsProps): ReactNode {
   const { socialMedias } = useSocialMediaStore();
+  const {
+    containerRef,
+    firstGradient,
+    handleMouseEnter,
+    handleMouseLeave,
+    lastGradient,
+    scrollToElement,
+  } = useHorizontalScroll();
 
   const tabClasses = (id: TabId): string =>
     classNames(scss.tab, id === props.currentTab.id && scss.active);
@@ -19,11 +28,20 @@ function Tabs(props: TabsProps): ReactNode {
   const renderSocialMediaIcon = (account: Account): ReactNode =>
     socialMedias.get(account.socialMediaId)?.icon;
 
+  const handleTabClick = (
+    tab: Tab,
+    tabId: TabId,
+    tabElement: HTMLElement
+  ): void => {
+    props.onChangeTab(tab, tabId);
+    scrollToElement(tabElement);
+  };
+
   const renderTabs = (tabId: TabId, tab: Tab): ReactNode => (
     <div
       className={tabClasses(tabId)}
       key={tabId}
-      onClick={() => props.onChangeTab(tab, tabId)}
+      onClick={(e) => handleTabClick(tab, tabId, e.currentTarget)}
     >
       {renderSocialMediaIcon(tab.account as Account)}
       <span className={scss.tabTitle}>{tab.account.userName}</span>
@@ -31,10 +49,19 @@ function Tabs(props: TabsProps): ReactNode {
   );
 
   return (
-    <div className={scss.tabsContainer}>
-      {Object.entries(props.tabs).map(([tabId, tab]) =>
-        renderTabs(tabId as TabId, tab)
-      )}
+    <div className={scss.tabsWrapper}>
+      <span className={scss.firstGradient} ref={firstGradient} />
+      <div
+        className={scss.tabsContainer}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        ref={containerRef}
+      >
+        {Object.entries(props.tabs).map(([tabId, tab]) =>
+          renderTabs(tabId as TabId, tab)
+        )}
+      </div>
+      <span className={scss.lastGradient} ref={lastGradient} />
     </div>
   );
 }
---
+++ src/hooks/useHorizontalScroll/useHorizontalScroll.ts
@@ -0,0 +1,120 @@
+import { RefObject, useCallback, useEffect, useRef } from 'react';
+
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/all';
+
+gsap.registerPlugin(ScrollToPlugin);
+
+const TWO = 2;
+
+const updateGradientVisibility = (
+  element: RefObject<HTMLDivElement>,
+  condition: boolean
+): void => {
+  if (element.current) {
+    element.current.style.display = condition ? 'block' : 'none';
+  }
+};
+
+export const useHorizontalScroll = (): {
+  containerRef: RefObject<HTMLDivElement>;
+  firstGradient: RefObject<HTMLDivElement>;
+  handleMouseEnter: () => void;
+  handleMouseLeave: () => void;
+  lastGradient: RefObject<HTMLDivElement>;
+  scrollToElement: (element: HTMLElement) => void;
+} => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const firstGradient = useRef<HTMLDivElement>(null);
+  const lastGradient = useRef<HTMLDivElement>(null);
+
+  const updateScrollStatus = useCallback(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+      const hasMoreOnLeft = container.scrollLeft > 0;
+      const hasMoreOnRight = container.scrollLeft < maxScrollLeft;
+
+      updateGradientVisibility(firstGradient, hasMoreOnLeft);
+      updateGradientVisibility(lastGradient, hasMoreOnRight);
+    }
+  }, []);
+
+  const scrollHorizontally = useCallback(
+    (e: WheelEvent) => {
+      e.preventDefault();
+      const scrollAmount = e.deltaY;
+      const container = containerRef.current;
+
+      if (container) {
+        const newScrollPosition = container.scrollLeft + scrollAmount;
+        gsap.to(container, {
+          duration: 0.5,
+          ease: 'power3.out',
+          onComplete: updateScrollStatus,
+          scrollTo: { x: newScrollPosition },
+        });
+      }
+    },
+    [updateScrollStatus]
+  );
+
+  const handleMouseEnter = useCallback(() => {
+    window.addEventListener('wheel', scrollHorizontally, { passive: false });
+  }, [scrollHorizontally]);
+
+  const handleMouseLeave = useCallback(() => {
+    window.removeEventListener('wheel', scrollHorizontally);
+  }, [scrollHorizontally]);
+
+  const scrollToElement = useCallback(
+    (element: HTMLElement) => {
+      const container = containerRef.current;
+
+      if (container) {
+        const containerRect = container.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
+
+        const offset = elementRect.left - containerRect.left;
+        const newScrollPosition =
+          container.scrollLeft +
+          offset -
+          container.clientWidth / TWO +
+          elementRect.width / TWO;
+
+        gsap.to(container, {
+          duration: 0.6,
+          ease: 'power3.out',
+          onComplete: updateScrollStatus,
+          scrollTo: { x: newScrollPosition },
+        });
+      }
+    },
+    [updateScrollStatus]
+  );
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      const observer = new MutationObserver(updateScrollStatus);
+      observer.observe(container, { childList: true, subtree: true });
+
+      updateScrollStatus();
+
+      return (): void => {
+        observer.disconnect();
+      };
+    }
+  }, [containerRef, updateScrollStatus]);
+
+  return {
+    containerRef,
+    firstGradient,
+    handleMouseEnter,
+    handleMouseLeave,
+    lastGradient,
+    scrollToElement,
+  };
+};
