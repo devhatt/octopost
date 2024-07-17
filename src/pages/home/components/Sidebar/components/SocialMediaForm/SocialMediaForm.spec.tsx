6034c3e6e83788bdc0b5ea5ae793f6674740ae18
Douglas Murita <douglasb008@gmail.com>
2024-07-17 20:32:38 -0300

    test: function props handle modal and promise userEvent fix

--- src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.spec.tsx
+++ src/pages/home/components/Sidebar/components/SocialMediaForm/SocialMediaForm.spec.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import SocialMediaForm from './SocialMediaForm';
 
 const setIsOpenMock = (): boolean => !true;
 
-describe('Social Media Form', () => {
+describe.only('Social Media Form', () => {
   it('renders correctly', () => {
     const { container } = render(
       <SocialMediaForm
@@ -15,5 +16,15 @@ describe('Social Media Form', () => {
     );
 
     expect(container).toBeDefined();
+  });
+  it('calls onClick prop when clicked', async () => {
+    const handleClick = vi.fn();
+
+    render(<SocialMediaForm onHandleToggleModal={handleClick} />);
+
+    const button = screen.getAllByRole('button');
+    await userEvent.click(button[0]);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
