9779f042f7d58072b27ffacb1736dddfd456d239
DominMFD <mfdmatheus@hotmail.com>
2024-08-13 21:16:36 -0300

    refactor: refactor post store

---
+++ src/stores/usePost/usePost.ts
@@ -0,0 +1,32 @@
+import { nanoid } from 'nanoid';
+
+import { StoreAccount } from '~stores/useSocialMediaStore/useSocialMediaStore.types';
+
+import { create } from '../zustand';
+
+import { PostModes, PostStore } from './usePost.types';
+
+export const usePostStore = create<PostStore>((set) => ({
+  addPost: (account, postsModes): void => {
+
+    const id = nanoid();
+    const postModes = postsModes.reduce<PostModes>((acc, postMode) => {
+      acc[postMode.id] = {}
+      return acc
+    }, {})
+
+    set((state) => ({
+      posts: {
+        ...state.posts, 
+        [id]: {
+          accountId: account.id,
+          id: id,
+          postModes,
+          socialMediaId: account.socialMediaId
+        }
+      },
+    }));
+  },
+
+  posts: {},
+}));
