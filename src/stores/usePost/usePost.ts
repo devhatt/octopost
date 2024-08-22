e2396c1043fcf8fb0a43166901100989e47929c3
juliaam <juliademoraess@gmail.com>
2024-08-21 21:55:34 -0300

    feat: add usepoststore

--- src/stores/usePost/usePost.ts
+++ src/stores/usePost/usePost.ts
@@ -10,19 +10,19 @@ export const usePostStore = create<PostStore>((set) => ({
   add: (account, postsModes): void => {
     const id = nanoid();
     const postModes = postsModes.reduce<PostModes>((acc, postMode) => {
-      acc[postMode.id] = {}
-      return acc
-    }, {})
+      acc[postMode.id] = {};
+      return acc;
+    }, {});
 
     set((state) => ({
       posts: {
-        ...state.posts, 
+        ...state.posts,
         [id]: {
           accountId: account.id,
           id: id,
           postModes,
-          socialMediaId: account.socialMediaId
-        }
+          socialMediaId: account.socialMediaId,
+        },
       },
     }));
   },
@@ -30,31 +30,30 @@ export const usePostStore = create<PostStore>((set) => ({
   mainContent: '',
   posts: {},
 
-  remove: (id): void => {
+  remove: (postId): void => {
     set((state) => {
-      const posts = omit(state.posts, id)
-      return { posts }
-    })
+      const posts = omit(state.posts, postId);
+      return { posts };
+    });
   },
 
   updateMainContent: (newContent: string): void =>
     set({ mainContent: newContent }),
 
-  updateText: ({postId, postModeId, text}): void => {
+  updateText: ({ postId, postModeId, text }): void => {
     set((state) => {
       const result = merge({}, state, {
         posts: {
           [postId]: {
             postModes: {
               [postModeId]: {
-                text
-              } 
-            }
-          }
-        }
-      })
-      return result
-    })  
-  }
+                text,
+              },
+            },
+          },
+        },
+      });
+      return result;
+    });
+  },
 }));
-
--- src/stores/usePost/usePost.types.ts
+++ src/stores/usePost/usePost.types.ts
@@ -1,34 +1,37 @@
-import { Account } from "~services/api/accounts/accounts.types";
-import { PostMode, SocialMedia } from "~services/api/social-media/social-media.types";
-import { StoreAccount } from "~stores/useSocialMediaStore/useSocialMediaStore.types";
+import { Account } from '~services/api/accounts/accounts.types';
+import {
+  PostMode,
+  SocialMedia,
+} from '~services/api/social-media/social-media.types';
+import { StoreAccount } from '~stores/useSocialMediaStore/useSocialMediaStore.types';
 
-import { IMedia } from "~components/MainComposer/components/InputMediaGroup/components/InputMedia/InputMedia.types";
+import { IMedia } from '~components/MainComposer/components/InputMediaGroup/components/InputMedia/InputMedia.types';
 
 export type MainContent = {
   medias?: IMedia[];
   text?: string;
 };
 
-export type PostModes = Record<PostMode['id'], MainContent>
+export type PostModes = Record<PostMode['id'], MainContent>;
 
 export type DataPost = {
-  accountId: Account['id']
-  id: string,
-  postModes: PostModes,
-  socialMediaId: SocialMedia['id'],
-}
+  accountId: Account['id'];
+  id: string;
+  postModes: PostModes;
+  socialMediaId: SocialMedia['id'];
+};
 
 type UpdateText = {
   postId: DataPost['id'];
   postModeId: PostMode['id'];
   text: string;
-}
+};
 
 export type PostStore = {
   add: (account: StoreAccount, postsModes: PostMode[]) => void;
-  mainContent: string,
+  mainContent: string;
   posts: Record<string, DataPost>;
-  remove: (id: string) => void,
-  updateMainContent: (newContent: string) => void,
-  updateText: ({postId, postModeId, text}: UpdateText) => void
+  remove: (postId: string) => void;
+  updateMainContent: (newContent: string) => void;
+  updateText: ({ postId, postModeId, text }: UpdateText) => void;
 };
