37f207f127357b30de5ea250af33371d8d7b7521
juliaam <juliademoraess@gmail.com>
2024-08-23 17:31:16 -0300

    chore: base for e2e

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,6 +3,10 @@ import { expect, test } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect a title "to contain" a substring.
+  const discordAccordion = page.getByText('Discord');
+
+  await discordAccordion.click();
+  await page.waitForTimeout(5000);
+
   await expect(page).toHaveTitle(/Octopost/);
 });
