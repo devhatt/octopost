9ca6afc48725407ca872cb662712d5dd6ef82a15
Luis Felipe <luis.felipeszsv@gmail.com>
2024-09-10 16:41:59 -0300

    fix: only allow action bar button when at least one account is selected

--- src/pages/home/components/ActionBar/ActionBar.tsx
+++ src/pages/home/components/ActionBar/ActionBar.tsx
@@ -1,21 +1,28 @@
 import { ReactNode } from 'react';
 
+import { useAccountStore } from '~stores/useAccountStore/useAccountStore';
+
 import Button from '~components/Button/Button';
 import Icon from '~components/Icon/Icon';
 
 import scss from './ActionBar.module.scss';
 
 function ActionBar(): ReactNode {
+  const { accounts } = useAccountStore();
+
   return (
     <div className={scss.container}>
       <div className={scss.composeContainer}>
         <Icon className={scss.closeIcon} icon="close" size={14} />
         <p className={scss.composeLabel}>Compose</p>
       </div>
-      <button className={scss.navigationIconContainer}>
-        <Icon className={scss.arrowIcon} icon="arrow-right" size={12} />
-      </button>
-
+      <Button
+        circle
+        className={scss.navigationIconContainer}
+        color="secondary"
+        disabled={accounts.length === 0}
+        icon={<Icon className={scss.arrowIcon} icon="arrow-right" size={12} />}
+      />
       <Button className={scss.submit} variant="container">
         Postar
       </Button>
